a0cd284449fbd22c4f95d57c5596cec6228adabe
Conor Watson <conor.watson94@gmail.com>
2020-02-03 18:55:30 +0000

    feat: add mongoose and a database connection helper

---
+++ wedding-api/src/lib/database.js
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cachedDb = null;
+
+const connectToDatabase = async () => {
+  try {
+    if (cachedDb == null) {
+      console.log('Creating new database connection');
+      cachedDb = await mongoose.createConnection(MONGODB_URI, {
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        poolSize: 10,
+        useUnifiedTopology: true,
+      });
+    }
+    return cachedDb;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export default { connectToDatabase };
