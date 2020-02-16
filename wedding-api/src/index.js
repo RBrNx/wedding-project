9df67acd3ec4182899f9c52fedeca74c2414a6ab
Conor Watson <conor.watson94@gmail.com>
2020-02-16 09:37:10 +0000

    chore: rename graphql.js to index.js and schema.js to typeDefs.js

--- wedding-api/src/index.js
+++ wedding-api/src/index.js
@@ -1,5 +1,5 @@
-import { ApolloServer, gql } from 'apollo-server-lambda';
-import schema from './schema';
+import { ApolloServer } from 'apollo-server-lambda';
+import schema from './typeDefs';
 import resolvers from './resolvers';
 
 const server = new ApolloServer({
