3110a37e9d459df691aaafe27d696f439a1ac4bc
Conor Watson <conor.watson94@gmail.com>
2020-02-18 22:00:19 +0000

    feat: rewrite createSeedData to insert directly to the DB

--- wedding-api/src/lib/createSeedData.js
+++ wedding-api/src/lib/createSeedData.js
@@ -1,34 +1,52 @@
-const faker = require('faker');
-const jsonfile = require('jsonfile');
+import _ from './env';
+import faker from 'faker';
+import { connectToDatabase } from './database';
 
 faker.seed(100);
 
-const guests = [];
+const NUM_INVITATIONS = 75;
 const invitations = [];
-const numOfInvitations = 75;
 
-for (let i = 0; i < numOfInvitations; i++) {
-  const numOfGuests = faker.random.number({ min: 1, max: 3 });
-  const invitation = {
+const generateGuest = () => {
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    attending: faker.random.boolean(),
+    mainCourse: faker.random.word(),
+  };
+};
+
+const generateInvitation = guests => {
+  return {
     uniqueCode: faker.random.alphaNumeric(6),
-    guests: [],
+    guests: guests.map(guest => guest._id),
+  };
+};
+
+const createWeddingGuests = async () => {
+  const db = await connectToDatabase();
+  const GuestSchema = db.model('Guest');
+  const InvitationSchema = db.model('Invitation');
+
+  for (let inv = 0; inv < NUM_INVITATIONS; inv++) {
+    const tempGuests = [];
+    const numOfGuests = faker.random.number({ min: 1, max: 3 });
+
+    for (let i = 0; i < numOfGuests; i++) {
+      const guest = generateGuest();
+      tempGuests.push(guest);
+    }
+
+    const dbGuests = await GuestSchema.insertMany(tempGuests);
+    const invitation = generateInvitation(dbGuests);
+    invitations.push(invitation);
   }
 
-  for(let g = 0; g < numOfGuests; g++){
-    const person = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      attending: faker.random.boolean(),
-      mainCourse: faker.random.word(),
-      id: faker.random.uuid(),
-    };
-    
-    invitation.guests.push(person.id);
-    guests.push(person);
-  }
+  await InvitationSchema.insertMany(invitations);
 
-  invitations.push(invitation);
-}
+  console.log(`Created ${invitations.length} invitations.`);
 
-jsonfile.writeFileSync('Guests.json', guests);
-jsonfile.writeFileSync('Invitations.json', invitations);
+  return;
+};
+
+createWeddingGuests();
