177a67efbafceffedf3cf8ea815f41f2aa0a7c91
Conor Watson <conor.watson94@gmail.com>
2020-02-22 10:57:04 +0000

    feat:  add try catch blocks to resolvers

--- wedding-api/src/resolvers.js
+++ wedding-api/src/resolvers.js
@@ -5,33 +5,46 @@ const hello = (args, context) => {
 };
 
 const getAllGuests = async (args, context) => {
-  const db = await connectToDatabase();
-  const GuestSchema = db.model('Guest');
-
-  const guests = await GuestSchema.find().exec();
-
-  return guests;
+  try {
+    const db = await connectToDatabase();
+    const GuestSchema = db.model('Guest');
+  
+    const guests = await GuestSchema.find().exec();
+  
+    return guests;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getAllInvitations = async (args, context) => {
-  const db = await connectToDatabase();
-  const InvitationSchema = db.model('Invitation');
-
-  const invitations = await InvitationSchema.find()
-    .populate('guests')
-    .exec();
-
-  return invitations;
+  try {
+    const db = await connectToDatabase();
+    const InvitationSchema = db.model('Invitation');
+  
+    const invitations = await InvitationSchema.find()
+      .populate('guests')
+      .exec();
+  
+    return invitations;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getInvitation = async (parent, args, context) => {
-  const { uniqueCode } = args;
-  const db = await connectToDatabase();
-  const InvitationSchema = db.model('Invitation');
-
-  const invitation = await InvitationSchema.findOne({ uniqueCode }).populate('guests').exec()
-
-  return invitation;
+  try {
+    const { uniqueCode } = args;
+    const db = await connectToDatabase();
+    const InvitationSchema = db.model('Invitation');
+  
+    const invitation = await InvitationSchema.findOne({ uniqueCode }).populate('guests').exec()
+  
+    return invitation;
+  }
+  catch(error){
+    return error;
+  }
 }
 
 export default {
