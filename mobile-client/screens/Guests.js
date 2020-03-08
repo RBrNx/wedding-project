92c01216413e6426171509cac7ec05a7fa105bf8
Conor Watson <conor.watson94@gmail.com>
2020-03-08 15:30:42 +0000

    feat: implement basic query in Guests.js

--- mobile-client/screens/Guests.js
+++ mobile-client/screens/Guests.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { loader } from 'graphql.macro';
+import { useQuery } from '@apollo/react-hooks';
 
-const GuestsScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 18 }}>Guests</Text>
-  </View>
-);
+const ALL_GUESTS_QUERY = loader('../graphql/allGuestsQuery.graphql');
+
+const GuestsScreen = () => {
+  const { loading, error, data } = useQuery(ALL_GUESTS_QUERY);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error</Text>;
+
+  const guests = data.getAllGuests;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      {guests.map(guest => {
+        return <Text key={guest._id}>{guest.name}</Text>;
+      })}
+    </View>
+  );
+};
 
 export default GuestsScreen;
