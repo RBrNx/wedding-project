40517108f0e7c710c261e06a61559671674ff5fc
Conor Watson <conor.watson94@gmail.com>
2020-03-09 23:08:20 +0000

    feat: implement ScrollViewAnimatedHeader in Guests screen

--- mobile-client/screens/Guests.js
+++ mobile-client/screens/Guests.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { loader } from 'graphql.macro';
 import { useQuery } from '@apollo/react-hooks';
+import Animated from 'react-native-reanimated';
 import GuestCard from '../components/GuestCard';
+import ScrollViewAnimatedHeader from '../components/ScrollViewAnimatedHeader';
+import GuestImage from '../assets/undraw_sign_in_e6hj.png';
 
 const ALL_GUESTS_QUERY = loader('../graphql/allGuestsQuery.graphql');
 
 const GuestsScreen = () => {
   const { loading, error, data } = useQuery(ALL_GUESTS_QUERY);
 
-  if (loading) return <Text>Loading...</Text>;
-  if (error) return <Text>Error</Text>;
-
-  const guests = data.getAllGuests;
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: '4%', backgroundColor: '#fff' }}>
-      {guests.map(guest => {
-        return <GuestCard key={guest._id} guest={guest} />;
-      })}
-    </View>
+    <ScrollViewAnimatedHeader title='Guests' imageSource={GuestImage}>
+      {loading && <Text>Loading...</Text>}
+      {error && <Text>Error</Text>}
+      {!loading && !error && (
+        <View style={{ flex: 1, alignItems: 'center', paddingHorizontal: '4%', backgroundColor: '#fff', paddingTop: 20 }}>
+          {data.getAllGuests.map(guest => {
+            return <GuestCard key={guest._id} guest={guest} />;
+          })}
+        </View>
+      )}
+    </ScrollViewAnimatedHeader>
   );
 };
 
