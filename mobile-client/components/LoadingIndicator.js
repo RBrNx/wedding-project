f0c8380780e61c9777341dde7907e85242be17f6
Conor Watson <conor.watson94@gmail.com>
2020-03-29 16:27:41 +0100

    fix: styles for LoadingIndicator

--- mobile-client/components/LoadingIndicator.js
+++ mobile-client/components/LoadingIndicator.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import LottieView from 'lottie-react-native';
+import { StyleSheet } from 'react-native';
 import LoadingAnimation from '../assets/animations/loading.json';
 
-const LoadingIndicator = () => <LottieView source={LoadingAnimation} autoPlay loop />;
+const LoadingIndicator = () => <LottieView source={LoadingAnimation} autoPlay loop style={styles.animation} speed={0.75} />;
+
+const styles = StyleSheet.create({
+  animation: {
+    marginTop: -30,
+  },
+});
 
 export default LoadingIndicator;
