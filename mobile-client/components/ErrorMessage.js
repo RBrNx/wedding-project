ff7ea654cf9ac268dc0cbf824f24c313af873453
Conor Watson <conor.watson94@gmail.com>
2020-03-29 16:27:07 +0100

    feat: replaced error animation with space animation and tweaked error message styles

--- mobile-client/components/ErrorMessage.js
+++ mobile-client/components/ErrorMessage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
-import ErrorAnimation from '../assets/animations/error.json';
+import ErrorAnimation from '../assets/animations/space.json';
 
 const ErrorMessage = ({ message }) => {
   const errorMessage = message || 'Whoops, something has went wrong!';
 
   return (
     <View style={styles.container}>
-      <LottieView source={ErrorAnimation} autoPlay loop={false} style={styles.animation} />
+      <LottieView source={ErrorAnimation} autoPlay style={styles.animation} />
       <Text style={styles.text}>{errorMessage}</Text>
     </View>
   );
@@ -21,13 +21,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   animation: {
-    height: 200,
-    marginTop: -50,
+    height: 150,
+    marginTop: -35,
   },
   text: {
-    marginTop: -75,
+    marginTop: -25,
     textAlign: 'center',
     fontSize: 16,
+    color: '#8C8C8C',
   },
 });
 
