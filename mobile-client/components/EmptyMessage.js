05ba78098443fe0b554f11730d4ff29f773cd1aa
Conor Watson <conor.watson94@gmail.com>
2020-03-29 16:26:38 +0100

    feat: create EmptyMessage component with new moon animation

---
+++ mobile-client/components/EmptyMessage.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, Animated } from 'react-native';
+import LottieView from 'lottie-react-native';
+import EmptyAnimation from '../assets/animations/moon.json';
+
+const EmptyMessage = ({ message }) => {
+  const [opacity] = useState(new Animated.Value(0));
+  const emptyMessage = message || "There doesn't seem to be anything here";
+
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [opacity]);
+
+  return (
+    <Animated.View style={[styles.container, { opacity }]}>
+      <LottieView source={EmptyAnimation} autoPlay style={styles.animation} speed={0.65} />
+      <Text style={styles.text}>{emptyMessage}</Text>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animation: {
+    marginTop: -50,
+  },
+  text: {
+    marginTop: 100,
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#8C8C8C',
+  },
+});
+
+export default EmptyMessage;
