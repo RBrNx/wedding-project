a3746030d8c4e2b45254b11fa09a73ef9fa934cb
Conor Watson <conor.watson94@gmail.com>
2020-04-12 14:24:59 +0100

    feat: set isAuthenticated in sign in screen

--- mobile-client/screens/SignIn.js
+++ mobile-client/screens/SignIn.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 
-const SignInScreen = ({ navigation }) => (
+const SignInScreen = ({ route }) => {
+  return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 18, marginBottom: 10 }}>SignIn</Text>
-        <Button title="Navigate to Home" onPress={() => navigation.navigate('Home')}/>
+      <Text style={{ fontSize: 18, marginBottom: 10 }}>SignIn</Text>
+      <Button title='Navigate to Home' onPress={() => route.params?.setIsAuthenticated(true)} />
     </View>
-);
+  );
+};
 
 export default SignInScreen;
