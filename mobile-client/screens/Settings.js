f4152bea3f410df6601fa4e077f4a8f4de90643a
Conor Watson <conor.watson@soar.tech>
2020-05-01 13:45:17 +0100

    feat: improved styling of Settings cards and option modal

--- mobile-client/screens/Settings.js
+++ mobile-client/screens/Settings.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, TouchableNativeFeedback } from 'react-native';
 import Modal from 'react-native-modal';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import FlatListAnimatedHeader from '../components/FlatListAnimatedHeader';
 import SettingsIllustration from '../components/SVG/Settings';
@@ -24,7 +25,7 @@ const SettingsScreen = () => {
   const { userSettings, saveUserSetting } = useSettings();
   const { colors } = useTheme();
 
-  const renderItem = ({ item }) => {
+  const renderSettingRow = ({ item }) => {
     const selectedOption = item.options.find(option => option.value === userSettings[item._id]);
 
     return (
@@ -34,7 +35,7 @@ const SettingsScreen = () => {
           setShowModal(true);
         }}
       >
-        <View style={styles.settingsRow}>
+        <View style={[styles.settingsRow, { backgroundColor: colors.card }]}>
           <Text style={{ color: colors.headerText }}>{item.title}</Text>
           <Text style={{ color: colors.bodyText }}>{selectedOption?.label}</Text>
         </View>
@@ -42,24 +43,37 @@ const SettingsScreen = () => {
     );
   };
 
+  const renderOptionRow = (option, isSelected) => {
+    return (
+      <TouchableNativeFeedback
+        onPress={() => {
+          setShowModal(false);
+          saveUserSetting(selectedSetting._id, option.value);
+        }}
+        key={option.value}
+      >
+        <View style={styles.optionRow}>
+          <Text style={{ color: colors.bodyText }}>{option.label}</Text>
+          <FontAwesome5 style={{ color: colors.bodyText }} name={isSelected ? 'dot-circle' : 'circle'} size={20} />
+        </View>
+      </TouchableNativeFeedback>
+    );
+  };
+
   return (
     <>
-      <FlatListAnimatedHeader title='Settings' renderImage={() => <SettingsIllustration size='90%' />} data={settings} renderItem={renderItem} />
+      <FlatListAnimatedHeader
+        title='Settings'
+        renderImage={() => <SettingsIllustration size='90%' />}
+        data={settings}
+        renderItem={renderSettingRow}
+      />
       <Modal isVisible={showModal} onBackdropPress={() => setShowModal(false)} useNativeDriver animationIn='fadeIn' animationOut='fadeOut'>
         <View style={[styles.modalContainer, { backgroundColor: colors.card }]}>
-          {selectedSetting?.options?.map(option => (
-            <TouchableNativeFeedback
-              onPress={() => {
-                setShowModal(false);
-                saveUserSetting(selectedSetting._id, option.value);
-              }}
-              key={option.value}
-            >
-              <View style={styles.optionRow}>
-                <Text>{option.label}</Text>
-              </View>
-            </TouchableNativeFeedback>
-          ))}
+          {selectedSetting?.options?.map(option => {
+            const isSelected = option.value === userSettings[selectedSetting._id];
+            return renderOptionRow(option, isSelected);
+          })}
         </View>
       </Modal>
     </>
@@ -72,18 +86,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
     padding: 20,
+    marginHorizontal: 20,
+    marginVertical: 5,
+    borderRadius: 10,
   },
   modalContainer: {
-    padding: 20,
+    paddingHorizontal: 20,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 4,
     borderColor: 'rgba(0, 0, 0, 0.1)',
   },
   optionRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingVertical: 20,
-    borderColor: '#ddd',
-    borderTopWidth: 1,
     width: '100%',
   },
 });
