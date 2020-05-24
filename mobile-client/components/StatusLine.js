f9e6f3693316bd64c742ee47e2132cde038f4885
Conor Watson <conor.watson94@gmail.com>
2020-05-24 13:43:07 +0100

    feat: use GuestResponseEnum in StatusLine component

--- mobile-client/components/StatusLine.js
+++ mobile-client/components/StatusLine.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-
-const statusColors = Object.freeze({ AWAITING_RSVP: '#2991cc', ATTENDING: '#04b84a', NOT_ATTENDING: '#966fd6' });
+import { GuestResponseEnum } from '../library/enums';
 
 const StatusLine = ({ status, isFirst = true, isLast = true }) => {
+  const { color } = GuestResponseEnum[status];
+
   return (
     <View
       style={[
         styles.statusLine,
         {
-          backgroundColor: statusColors[status],
+          backgroundColor: color,
           borderTopRightRadius: isFirst ? 2.5 : 0,
           borderTopLeftRadius: isFirst ? 2.5 : 0,
           borderBottomLeftRadius: isLast ? 2.5 : 0,
