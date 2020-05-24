a412d4376e72f6de8ff52de814ce6a030ac27009
Conor Watson <conor.watson94@gmail.com>
2020-05-24 13:29:44 +0100

    feat: default StatusLine to have rounded corners

--- mobile-client/components/StatusLine.js
+++ mobile-client/components/StatusLine.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from 'react-native';
 
 const statusColors = Object.freeze({ AWAITING_RSVP: '#2991cc', ATTENDING: '#04b84a', NOT_ATTENDING: '#966fd6' });
 
-const StatusLine = ({ status, isFirst, isLast }) => {
+const StatusLine = ({ status, isFirst = true, isLast = true }) => {
   return (
     <View
       style={[
