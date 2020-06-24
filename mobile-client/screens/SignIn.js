eaf5c4fe5fb8ad5637730880fe09f48b13a11246
Conor Watson <conor.watson94@gmail.com>
2020-06-24 10:57:33 +0100

    feat: add temp test query to SignIn screen

--- mobile-client/screens/SignIn.js
+++ mobile-client/screens/SignIn.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
+import { useQuery } from '@apollo/react-hooks';
+import { loader } from 'graphql.macro';
+
+const TEST_QUERY = loader('../graphql/test.graphql');
 
 const SignInScreen = ({ route }) => {
+  const { loading, error, data } = useQuery(TEST_QUERY, { variables: { uniqueCode: 'i6k307' } });
+
+  console.log({ loading, error, data });
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 18, marginBottom: 10 }}>SignIn</Text>
