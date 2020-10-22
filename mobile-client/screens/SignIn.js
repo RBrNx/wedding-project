02ed5a2e136f41906c647fe8fed6f1922ce338b9
Conor Watson <conor.watson94@gmail.com>
2020-10-22 16:58:18 +0100

    feat: added useAuth hook into SignIn screen

--- mobile-client/screens/SignIn.js
+++ mobile-client/screens/SignIn.js
@@ -1,42 +1,48 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Auth } from 'aws-amplify';
 import StandardButton from '../components/StandardButton';
 import StandardInput from '../components/StandardInput';
 import LandingScreenBackground from '../components/LandingScreenBackground';
+import { useAuth } from '../context';
+import DismissKeyboard from '../components/DismissKeyboard';
 
-const SignInScreen = ({ route }) => {
+const SignInScreen = () => {
   const [emailAddress, setEmailAddress] = useState(null);
   const [password, setPassword] = useState(null);
+  const [signingIn, setSigningIn] = useState(false);
+  const { signIn } = useAuth();
 
   const attemptLogin = async () => {
     try {
-      const lowercaseEmail = emailAddress.toLowerCase();
-      await Auth.signIn(lowercaseEmail, password);
+      setSigningIn(true);
+      await signIn(emailAddress, password);
 
       // eslint-disable-next-line no-unused-expressions
-      route.params?.setIsAuthenticated(true);
+      // route.params?.setIsAuthenticated(true);
     } catch (err) {
       console.log(err);
+      setSigningIn(false);
     }
   };
 
   return (
     <>
       <LandingScreenBackground />
-      <View style={styles.container}>
-        <View style={styles.headingContainer}>
-          <Text style={styles.heading}>Hello</Text>
-          <Text style={styles.subHeading}>Sign in to your Account</Text>
+      <DismissKeyboard>
+        <View style={styles.container}>
+          <View style={styles.headingContainer}>
+            <Text style={styles.heading}>Hello</Text>
+            <Text style={styles.subHeading}>Sign in to your Account</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <StandardInput value={emailAddress} label='Email Address' onChangeText={value => setEmailAddress(value)} />
+            <StandardInput value={password} label='Password' onChangeText={value => setPassword(value)} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <StandardButton text='Sign In' raised onPress={attemptLogin} loading={signingIn} />
+          </View>
         </View>
-        <View style={styles.inputContainer}>
-          <StandardInput value={emailAddress} label='Email Address' onChangeText={value => setEmailAddress(value)} />
-          <StandardInput value={password} label='Password' onChangeText={value => setPassword(value)} />
-        </View>
-        <View style={styles.buttonContainer}>
-          <StandardButton text='Sign In' raised onPress={attemptLogin} />
-        </View>
-      </View>
+      </DismissKeyboard>
     </>
   );
 };
