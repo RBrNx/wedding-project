4e6b237739c1ac088385bcfea7270b41f3f5ee67
Conor Watson <conor.watson94@gmail.com>
2020-11-25 22:03:49 +0000

    feat: created generatePassword and generateTemporaryCredentials helpers

--- wedding-api/src/lib/helpers.js
+++ wedding-api/src/lib/helpers.js
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+import { connectToDatabase } from './database';
 const mapEnumValues = enumObj => {
   return Object.keys(enumObj).map(key => key);
 };
@@ -7,4 +9,31 @@ const stripNonAlphaChars = string => {
   return string.replace(/\s+/g, '').replace(/'+/g, '').replace(/-+/g, '');
 };
 
-export { mapEnumValues, stripNonAlphaChars };
+const generatePassword = (
+  length = 15,
+  wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$',
+) => {
+  return Array.from(crypto.randomFillSync(new Uint32Array(length)))
+    .map(x => wishlist[x % wishlist.length])
+    .join('');
+};
+
+const generateTemporaryCredentials = async ({ firstName, lastName }) => {
+  const db = await connectToDatabase();
+  const TempLoginDetailsModel = db.model('TempLoginDetails');
+
+  const forename = stripNonAlphaChars(firstName);
+  const surname = stripNonAlphaChars(lastName);
+  let username = `${forename}${surname}`.toLowerCase();
+  const password = generatePassword();
+
+  const usernameCount = await TempLoginDetailsModel.countDocuments({ username: { $regex: `^${username}` } });
+  if (usernameCount > 0) username += usernameCount;
+
+  return {
+    username,
+    password,
+  };
+};
+
+export { mapEnumValues, stripNonAlphaChars, generateTemporaryCredentials };
