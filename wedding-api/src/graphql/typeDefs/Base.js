f4ca56c47e66a99b4cda49399bb432f23c0d296b
Conor Watson <conor.watson94@gmail.com>
2020-11-25 18:31:45 +0000

    feat: add custom scalars for Date, Time, DateTime, EmailAddress and URL

--- wedding-api/src/graphql/typeDefs/Base.js
+++ wedding-api/src/graphql/typeDefs/Base.js
@@ -1,6 +1,16 @@
 import { gql } from 'apollo-server-lambda';
 
 const schema = gql`
+  scalar Date
+
+  scalar Time
+
+  scalar DateTime
+
+  scalar EmailAddress
+
+  scalar URL
+
   interface MutationResponse {
     success: Boolean
     message: String
