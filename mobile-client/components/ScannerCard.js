87d1d45d71edccd19110178160f051e42aebc9d9
Conor Watson <conor.watson94@gmail.com>
2020-12-30 10:27:43 +0000

    feat: add Keyboard Mode to ScannerCard

--- mobile-client/components/ScannerCard.js
+++ mobile-client/components/ScannerCard.js
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 import StandardPressable from '../library/components/StandardPressable';
+import StandardInput from '../library/components/StandardInput';
 
-const ScannerCard = ({ onKeyboardPress, onFlashPress, onClose, flashEnabled }) => {
+const ScannerCard = ({ onFlashPress, flashEnabled, onClose }) => {
+  const [inKeyboardMode, setInKeyboardMode] = useState(false);
+  const [shortId, setShortId] = useState(null);
   const { colors } = useTheme();
 
   return (
     <View style={[styles.card, styles.scannerCard, { backgroundColor: colors.card }]}>
-      <StandardPressable
-        onPress={onKeyboardPress}
-        style={[styles.icon, { backgroundColor: colors.componentBackground }]}
-        pressedStyle={{ backgroundColor: colors.cardHover }}
-      >
-        <FontAwesome5 name='keyboard' size={24} color={colors.text} />
-      </StandardPressable>
-      <StandardPressable
-        onPress={onClose}
-        style={[styles.icon, { backgroundColor: colors.componentBackground }]}
-        pressedStyle={{ backgroundColor: colors.cardHover }}
-      >
-        <Ionicons name='close-outline' size={36} color={colors.text} />
-      </StandardPressable>
-      <StandardPressable
-        onPress={onFlashPress}
-        style={[styles.icon, { backgroundColor: colors.componentBackground }]}
-        pressedStyle={{ backgroundColor: colors.cardHover }}
-      >
-        <Ionicons name={flashEnabled ? 'flash-outline' : 'flash-off-outline'} size={24} color={colors.text} />
-      </StandardPressable>
+      {inKeyboardMode && (
+        <>
+          <StandardPressable
+            onPress={() => setInKeyboardMode(!inKeyboardMode)}
+            style={[styles.icon, { backgroundColor: colors.componentBackground, marginLeft: 15 }]}
+            pressedStyle={{ backgroundColor: colors.cardHover }}
+          >
+            <Ionicons name='arrow-back-outline' size={24} color={colors.text} />
+          </StandardPressable>
+          <StandardInput
+            value={shortId}
+            label='Invitation ID'
+            onChangeText={value => setShortId(value)}
+            maxLength={12}
+            containerStyle={{ flex: 1, marginHorizontal: 20 }}
+            inputStyle={{ marginBottom: 0 }}
+          />
+          <StandardPressable
+            onPress={() => setInKeyboardMode(!inKeyboardMode)}
+            style={[styles.icon, { backgroundColor: colors.componentBackground, marginRight: 15 }]}
+            pressedStyle={{ backgroundColor: colors.cardHover }}
+          >
+            <Ionicons name='enter-outline' size={24} color={colors.text} />
+          </StandardPressable>
+        </>
+      )}
+      {!inKeyboardMode && (
+        <>
+          <StandardPressable
+            onPress={() => setInKeyboardMode(!inKeyboardMode)}
+            style={[styles.icon, { backgroundColor: colors.componentBackground }]}
+            pressedStyle={{ backgroundColor: colors.cardHover }}
+          >
+            <FontAwesome5 name='keyboard' size={24} color={colors.text} />
+          </StandardPressable>
+          <StandardPressable
+            onPress={onClose}
+            style={[styles.icon, { backgroundColor: colors.componentBackground }]}
+            pressedStyle={{ backgroundColor: colors.cardHover }}
+          >
+            <Ionicons name='close-outline' size={36} color={colors.text} />
+          </StandardPressable>
+          <StandardPressable
+            onPress={onFlashPress}
+            style={[styles.icon, { backgroundColor: colors.componentBackground }]}
+            pressedStyle={{ backgroundColor: colors.cardHover }}
+          >
+            <Ionicons name={flashEnabled ? 'flash-outline' : 'flash-off-outline'} size={24} color={colors.text} />
+          </StandardPressable>
+        </>
+      )}
     </View>
   );
 };
@@ -43,6 +76,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   scannerCard: {
+    flex: 1,
     position: 'absolute',
     bottom: 0,
     flexDirection: 'row',
