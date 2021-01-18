4a8967fc47019e6551628935703717bdea8a24f6
Conor Watson <conor.watson94@gmail.com>
2021-01-18 21:45:12 +0000

    refactor: tidy up of StandardActionButton

--- mobile-client/library/components/StandardActionButton.js
+++ mobile-client/library/components/StandardActionButton.js
@@ -2,7 +2,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import Color from 'color';
 import React, { useState } from 'react';
-import { Dimensions, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Pressable, StyleSheet, View } from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -12,53 +12,29 @@ import Animated, {
   Easing,
   withSequence,
   withDelay,
-  interpolateColors,
   interpolateColor,
   useDerivedValue,
   runOnJS,
-  cancelAnimation,
 } from 'react-native-reanimated';
-import { constantStyles } from '../../styles/theming';
 
 const { width } = Dimensions.get('window');
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
-const StandardActionButton = ({ size = 56, icon, style }) => {
+const StandardActionButton = ({ size = 56, icon, expansionStyle, buttonStyle, messageStyle }) => {
+  const [isPressed, setIsPressed] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
   const actionButtonExpansion = useSharedValue(0);
   const { colors } = useTheme();
-  const backgroundColor = colors.secondary;
-  const pressedColour = Color(colors.secondary)
+  const closeColor = Color(colors.button)
     .lighten(0.2)
-    .toString();
+    .string();
+  const closePressedColor = Color(colors.button)
+    .lighten(0.1)
+    .string();
 
   const switchIcon = newState => {
     if (newState !== isExpanded) setIsExpanded(newState);
   };
-
-  useDerivedValue(() => {
-    if (actionButtonExpansion.value > 0.5 && !isExpanded) runOnJS(switchIcon)(true);
-    else if (actionButtonExpansion.value <= 0.5 && isExpanded) runOnJS(switchIcon)(false);
-  }, [isExpanded]);
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      width: interpolate(actionButtonExpansion.value, [0, 1], [size, width - width * 0.05 - 16], Extrapolate.CLAMP),
-    };
-  });
-  const animatedStyles2 = useAnimatedStyle(() => {
-    const rotate = interpolate(actionButtonExpansion.value, [0, 1], [0, 180], Extrapolate.CLAMP);
-    const bgColor = interpolateColor(actionButtonExpansion.value, [0, 1], [backgroundColor, pressedColour]);
-
-    return {
-      transform: [{ rotate: `${rotate}deg` }],
-      backgroundColor: bgColor,
-    };
-  });
-  const animatedStyles3 = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(actionButtonExpansion.value, [0, 0.65, 1], [0, 0, 1], Extrapolate.CLAMP),
-    };
-  });
 
   const showMessage = () => {
     const animateOut = withTiming(1, { duration: 500, easing: Easing.inOut(Easing.exp) });
@@ -70,57 +46,61 @@ const StandardActionButton = ({ size = 56, icon, style }) => {
     actionButtonExpansion.value = withTiming(0, { duration: 500, easing: Easing.inOut(Easing.exp) });
   };
 
-  // const renderButtonStyles = ({ pressed }) => {
-  //   const buttonStyles = [
-  //     styles.button,
-  //     {
-  //       width: size,
-  //       height: size,
-  //       borderRadius: size / 2,
-  //       right: 16,
-  //       bottom: 16,
-  //       backgroundColor: pressed ? pressedColour : backgroundColor,
-  //       // transform: [{ scaleX }],
-  //     },
-  //     style,
-  //   ];
+  useDerivedValue(() => {
+    if (actionButtonExpansion.value > 0.5 && !isExpanded) runOnJS(switchIcon)(true);
+    else if (actionButtonExpansion.value <= 0.5 && isExpanded) runOnJS(switchIcon)(false);
+  }, [isExpanded]);
 
-  //   if (raised) buttonStyles.push({ ...constantStyles.inputShadow });
+  const animatedExpansionStyle = useAnimatedStyle(() => {
+    return {
+      width: interpolate(actionButtonExpansion.value, [0, 1], [size, width - width * 0.05 - 16], Extrapolate.CLAMP),
+    };
+  });
 
-  //   return buttonStyles;
-  // };
+  const animatedButtonStyle = useAnimatedStyle(() => {
+    const rotate = interpolate(actionButtonExpansion.value, [0, 1], [0, 180], Extrapolate.CLAMP);
+    const bgColor = interpolateColor(
+      actionButtonExpansion.value,
+      [0, 1],
+      [isPressed ? colors.buttonPressed : colors.button, isPressed ? closePressedColor : closeColor],
+    );
+
+    return {
+      transform: [{ rotate: `${rotate}deg` }],
+      backgroundColor: bgColor,
+    };
+  });
+
+  const animatedMessageStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(actionButtonExpansion.value, [0, 0.65, 1], [0, 0, 1], Extrapolate.CLAMP),
+    };
+  });
 
   return (
-    <View style={styles.container}>
+    <View style={styles.fullscreenContainer}>
       <Animated.View
         style={[
+          styles.expansion,
           {
-            position: 'absolute',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: size,
             height: size,
             borderRadius: size / 2,
             right: 16,
             bottom: 16,
-            backgroundColor,
-            overflow: 'hidden',
+            backgroundColor: colors.button,
           },
-          animatedStyles,
+          animatedExpansionStyle,
+          expansionStyle,
         ]}
       >
         <Animated.Text
           style={[
+            styles.message,
             {
-              color: '#fff',
-              paddingLeft: 0,
-              flex: 1,
-              flexWrap: 'wrap',
-              marginLeft: 30,
               marginRight: size,
             },
-            animatedStyles3,
+            animatedMessageStyle,
+            messageStyle,
           ]}
         >
           Please select an answer before continuing.
@@ -129,14 +109,16 @@ const StandardActionButton = ({ size = 56, icon, style }) => {
           style={[
             styles.button,
             {
-              backgroundColor,
               width: size,
               height: size,
               borderRadius: size / 2,
             },
-            animatedStyles2,
+            animatedButtonStyle,
+            buttonStyle,
           ]}
           onPress={isExpanded ? closeMessage : showMessage}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
         >
           {isExpanded ? <Ionicons name='close-outline' size={36} color='#fff' /> : icon()}
         </AnimatedPressable>
@@ -146,9 +128,23 @@ const StandardActionButton = ({ size = 56, icon, style }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  fullscreenContainer: {
     ...StyleSheet.absoluteFillObject,
     zIndex: 99,
+  },
+  expansion: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  message: {
+    flex: 1,
+    flexWrap: 'wrap',
+    color: '#fff',
+    paddingLeft: 0,
+    marginLeft: 30,
   },
   button: {
     position: 'absolute',
