a3f213f8c9401c9fc7de902ccaa9a3c73e06cf03
Conor Watson <conor.watson94@gmail.com>
2021-01-18 21:16:20 +0000

    feat: second attempt at StandardActionButton animation with reanimated

--- mobile-client/library/components/StandardActionButton.js
+++ mobile-client/library/components/StandardActionButton.js
@@ -1,108 +1,146 @@
+import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import Color from 'color';
 import React, { useState } from 'react';
-import { Animated, Dimensions, Easing, Pressable, StyleSheet, View } from 'react-native';
+import { Dimensions, Pressable, StyleSheet, Text, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+  Extrapolate,
+  Easing,
+  withSequence,
+  withDelay,
+  interpolateColors,
+  interpolateColor,
+  useDerivedValue,
+  runOnJS,
+  cancelAnimation,
+} from 'react-native-reanimated';
 import { constantStyles } from '../../styles/theming';
 
 const { width } = Dimensions.get('window');
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
-const StandardActionButton = ({ size = 56, raised, icon, style }) => {
+const StandardActionButton = ({ size = 56, icon, style }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [actionButtonAnimation] = useState(new Animated.Value(0));
+  const actionButtonExpansion = useSharedValue(0);
   const { colors } = useTheme();
   const backgroundColor = colors.secondary;
   const pressedColour = Color(colors.secondary)
-    .darken(0.2)
+    .lighten(0.2)
     .toString();
 
-  const onPress = () => {
-    setIsExpanded(!isExpanded);
-    Animated.timing(actionButtonAnimation, {
-      toValue: isExpanded ? 0 : 1,
-      duration: 500,
-      useNativeDriver: true,
-      easing: Easing.inOut(Easing.exp),
-    }).start();
+  const switchIcon = newState => {
+    if (newState !== isExpanded) setIsExpanded(newState);
   };
 
-  const scaleValue = width - width * 0.05 - size - 16;
+  useDerivedValue(() => {
+    if (actionButtonExpansion.value > 0.5 && !isExpanded) runOnJS(switchIcon)(true);
+    else if (actionButtonExpansion.value <= 0.5 && isExpanded) runOnJS(switchIcon)(false);
+  }, [isExpanded]);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      width: interpolate(actionButtonExpansion.value, [0, 1], [size, width - width * 0.05 - 16], Extrapolate.CLAMP),
+    };
+  });
+  const animatedStyles2 = useAnimatedStyle(() => {
+    const rotate = interpolate(actionButtonExpansion.value, [0, 1], [0, 180], Extrapolate.CLAMP);
+    const bgColor = interpolateColor(actionButtonExpansion.value, [0, 1], [backgroundColor, pressedColour]);
 
-  const scaleX = actionButtonAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [1, scaleValue + 2],
-    extrapolate: 'clamp',
+    return {
+      transform: [{ rotate: `${rotate}deg` }],
+      backgroundColor: bgColor,
+    };
+  });
+  const animatedStyles3 = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(actionButtonExpansion.value, [0, 0.65, 1], [0, 0, 1], Extrapolate.CLAMP),
+    };
   });
 
-  const translateXSide = actionButtonAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, -scaleValue + 1],
-    extrapolate: 'clamp',
-  });
-
-  const translateX = actionButtonAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, -(scaleValue / 2)],
-    extrapolate: 'clamp',
-  });
-
-  // const backgroundColor = actionButtonAnimation.interpolate({
-  //   inputRange: [0, 1],
-  //   outputRange: ['rgba(255,0,0,1)', 'rgba(0,255,0,1)'],
-  //   extrapolate: 'clamp',
-  // });
-
-  const renderButtonStyles = ({ pressed }) => {
-    const buttonStyles = [
-      styles.button,
-      {
-        width: size,
-        height: size,
-        borderRadius: size / 2,
-        right: 16,
-        bottom: 16,
-        backgroundColor: pressed ? pressedColour : backgroundColor,
-        // transform: [{ scaleX }],
-      },
-      style,
-    ];
-
-    if (raised) buttonStyles.push({ ...constantStyles.inputShadow });
-
-    return buttonStyles;
+  const showMessage = () => {
+    const animateOut = withTiming(1, { duration: 500, easing: Easing.inOut(Easing.exp) });
+    const animateIn = withTiming(0, { duration: 500, easing: Easing.inOut(Easing.exp) });
+    actionButtonExpansion.value = withSequence(animateOut, withDelay(3000, animateIn));
   };
+
+  const closeMessage = () => {
+    actionButtonExpansion.value = withTiming(0, { duration: 500, easing: Easing.inOut(Easing.exp) });
+  };
+
+  // const renderButtonStyles = ({ pressed }) => {
+  //   const buttonStyles = [
+  //     styles.button,
+  //     {
+  //       width: size,
+  //       height: size,
+  //       borderRadius: size / 2,
+  //       right: 16,
+  //       bottom: 16,
+  //       backgroundColor: pressed ? pressedColour : backgroundColor,
+  //       // transform: [{ scaleX }],
+  //     },
+  //     style,
+  //   ];
+
+  //   if (raised) buttonStyles.push({ ...constantStyles.inputShadow });
+
+  //   return buttonStyles;
+  // };
 
   return (
     <View style={styles.container}>
-      <Pressable style={renderButtonStyles} onPress={onPress}>
-        <Animated.View
-          style={{
-            borderTopLeftRadius: size / 2,
-            borderBottomLeftRadius: size / 2,
-            backgroundColor,
-            width: size / 2,
+      <Animated.View
+        style={[
+          {
+            position: 'absolute',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: size,
             height: size,
-            transform: [{ translateX: translateXSide }],
-          }}
-        />
-        <Animated.View
-          style={{
-            width: 1,
-            height: size,
+            borderRadius: size / 2,
+            right: 16,
+            bottom: 16,
             backgroundColor,
-            // opacity: 0.5,
-            transform: [{ translateX }, { scaleX }],
-          }}
-        />
-        <View
-          style={{
-            borderTopRightRadius: size / 2,
-            borderBottomRightRadius: size / 2,
-            backgroundColor,
-            width: size / 2,
-            height: size,
-          }}
-        />
-      </Pressable>
+            overflow: 'hidden',
+          },
+          animatedStyles,
+        ]}
+      >
+        <Animated.Text
+          style={[
+            {
+              color: '#fff',
+              paddingLeft: 0,
+              flex: 1,
+              flexWrap: 'wrap',
+              marginLeft: 30,
+              marginRight: size,
+            },
+            animatedStyles3,
+          ]}
+        >
+          Please select an answer before continuing.
+        </Animated.Text>
+        <AnimatedPressable
+          style={[
+            styles.button,
+            {
+              backgroundColor,
+              width: size,
+              height: size,
+              borderRadius: size / 2,
+            },
+            animatedStyles2,
+          ]}
+          onPress={isExpanded ? closeMessage : showMessage}
+        >
+          {isExpanded ? <Ionicons name='close-outline' size={36} color='#fff' /> : icon()}
+        </AnimatedPressable>
+      </Animated.View>
     </View>
   );
 };
@@ -114,7 +152,8 @@ const styles = StyleSheet.create({
   },
   button: {
     position: 'absolute',
-    // justifyContent: 'center',
+    right: 0,
+    justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
   },
