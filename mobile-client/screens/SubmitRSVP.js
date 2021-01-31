7ee6353dccb7b40677d0640c9bc83b2b494185ae
Conor Watson <conor.watson94@gmail.com>
2021-01-31 09:52:34 +0000

    feat: added FormOverview screen to SubmitRSVP and add wizard animation

--- mobile-client/screens/SubmitRSVP.js
+++ mobile-client/screens/SubmitRSVP.js
@@ -4,10 +4,14 @@ import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View, Image, Dimensions } from 'react-native';
 import { loader } from 'graphql.macro';
 import { useQuery } from '@apollo/react-hooks';
+import Animated from 'react-native-reanimated';
 import LoadingIndicator from '../components/LoadingIndicator';
 import FormWizard from '../library/components/FormWizard';
 import StandardActionButton from '../library/components/StandardActionButton';
 import { calculateQuestions } from '../library/helpers/RSVP';
+import DismissKeyboard from '../components/DismissKeyboard';
+import FormOverview from '../library/components/FormOverview';
+import useAnimatedWizard from '../library/hooks/useAnimatedWizard';
 
 const { width } = Dimensions.get('window');
 const GET_RSVP_QUESTIONS = loader('../graphql/getRSVPQuestions.graphql');
@@ -18,11 +22,13 @@ const SubmitRSVPScreen = ({ navigation }) => {
   const [questionNumber, setQuestionNumber] = useState(1);
   const [rsvpForm, setRSVPForm] = useState({});
   const [formError, setFormError] = useState(null);
+  const [showOverview, setShowOverview] = useState(false);
   const { loading, error, data } = useQuery(GET_RSVP_QUESTIONS);
   const { colors } = useTheme();
 
   const { getRSVPQuestions: questions } = data || {};
   const currAnswer = rsvpForm[currQuestion?._id];
+  const { animateStepChange: animateRSVPStep, animatedWizardStyle } = useAnimatedWizard();
   const { prevQuestion, nextQuestion } = calculateQuestions({
     questions,
     questionHistory,
@@ -34,7 +40,7 @@ const SubmitRSVPScreen = ({ navigation }) => {
     navigation.setOptions({
       headerLeft: () => <HeaderBackButton tintColor='#fff' onPress={onPrev} />,
     });
-  }, [navigation, currQuestion]);
+  }, [navigation, currQuestion, showOverview]);
 
   useEffect(() => {
     if (questions) setCurrQuestion({ ...questions[0], number: questionNumber });
@@ -46,15 +52,23 @@ const SubmitRSVPScreen = ({ navigation }) => {
       return;
     }
 
-    if (nextQuestion) {
+    if (showOverview) {
+      // Submit Answers
+    } else if (nextQuestion) {
       setQuestionHistory([...questionHistory, currQuestion]);
       setCurrQuestion({ ...nextQuestion, number: questionNumber + 1 });
       setQuestionNumber(questionNumber + 1);
+    } else {
+      setQuestionHistory([...questionHistory, currQuestion]);
+      animateRSVPStep({ fromStep: 0, toStep: 1, callback: () => setShowOverview(true) });
     }
   };
 
   const onPrev = () => {
-    if (prevQuestion) {
+    if (showOverview) {
+      setQuestionHistory(questionHistory.slice(0, -1));
+      animateRSVPStep({ fromStep: 1, toStep: 0, callback: () => setShowOverview(false) });
+    } else if (prevQuestion) {
       setQuestionHistory(questionHistory.slice(0, -1));
       setCurrQuestion({ ...prevQuestion });
       setQuestionNumber(questionNumber - 1);
@@ -67,12 +81,17 @@ const SubmitRSVPScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={[styles.card, { backgroundColor: colors.card }]}>
-        {loading && <LoadingIndicator size={100} />}
-        {!loading && !error && currQuestion && (
-          <FormWizard question={currQuestion} setFormValue={setFormValue} formValues={rsvpForm} />
-        )}
-      </View>
+      <DismissKeyboard>
+        <View style={[styles.card, { backgroundColor: colors.card }]}>
+          {loading && <LoadingIndicator size={100} />}
+          <Animated.View style={[styles.rsvpFormContainer, animatedWizardStyle]}>
+            {!loading && !error && currQuestion && !showOverview && (
+              <FormWizard question={currQuestion} setFormValue={setFormValue} formValues={rsvpForm} />
+            )}
+            {!loading && !error && showOverview && <FormOverview questions={questionHistory} formValues={rsvpForm} />}
+          </Animated.View>
+        </View>
+      </DismissKeyboard>
       <StandardActionButton
         icon={() => <Image source={Assets[0]} fadeDuration={0} style={styles.actionButtonIcon} />}
         maxExpansionWidth={width * 0.95 - 16}
@@ -100,6 +119,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     overflow: 'hidden',
+  },
+  rsvpFormContainer: {
+    width: '100%',
+    flex: 1,
   },
   actionButtonIcon: {
     tintColor: '#fff',
