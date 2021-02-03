6d7526ea924c822827e8e2e374580067523aca5a
Conor Watson <conor.watson94@gmail.com>
2021-02-03 21:42:18 +0000

    feat: add RSVPResponse typedefs and resolvers to respective arrays

--- wedding-api/src/graphql/index.js
+++ wedding-api/src/graphql/index.js
@@ -1,18 +1,37 @@
+// Resolvers
 import BaseResolvers from './resolvers/Base';
 import UserResolvers from './resolvers/User';
 import InvitationResolvers from './resolvers/InvitationGroup';
 import QuestionResolvers from './resolvers/Question';
 import TempLoginResolvers from './resolvers/TempLogin';
 import EventResolvers from './resolvers/Event';
+import RSVPResponseResolvers from './resolvers/RSVPResponse';
+
+// TypeDefs
 import BaseTypeDefs from './typeDefs/Base';
 import UserTypeDefs from './typeDefs/User';
 import InvitationTypeDefs from './typeDefs/InvitationGroup';
 import QuestionTypeDefs from './typeDefs/Question';
 import TempLoginTypeDefs from './typeDefs/TempLogin';
 import EventTypeDefs from './typeDefs/Event';
+import RSVPResponseTypeDefs from './typeDefs/RSVPResponse';
 
-const typeDefs = [BaseTypeDefs, UserTypeDefs, InvitationTypeDefs, QuestionTypeDefs, EventTypeDefs];
-const resolvers = [BaseResolvers, UserResolvers, InvitationResolvers, QuestionResolvers, EventResolvers];
+const typeDefs = [
+  BaseTypeDefs,
+  UserTypeDefs,
+  InvitationTypeDefs,
+  QuestionTypeDefs,
+  EventTypeDefs,
+  RSVPResponseTypeDefs,
+];
+const resolvers = [
+  BaseResolvers,
+  UserResolvers,
+  InvitationResolvers,
+  QuestionResolvers,
+  EventResolvers,
+  RSVPResponseResolvers,
+];
 
 const unauthenticatedTypeDefs = [BaseTypeDefs, TempLoginTypeDefs];
 const unauthenticatedResolvers = [BaseResolvers, TempLoginResolvers];
