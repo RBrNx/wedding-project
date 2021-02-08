59cf5fc74e9b5af56dc379240d4bbac58d4ecc7b
Conor Watson <conor.watson94@gmail.com>
2021-02-08 20:12:33 +0000

    feat: first working attempt at SubmitRSVP screen

--- mobile-client/screens/SubmitRSVP.js
+++ mobile-client/screens/SubmitRSVP.js
@@ -3,19 +3,21 @@ import { HeaderBackButton, Assets } from '@react-navigation/stack';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View, Image, Dimensions } from 'react-native';
 import { loader } from 'graphql.macro';
-import { useQuery } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/react-hooks';
 import Animated from 'react-native-reanimated';
 import LoadingIndicator from '../components/LoadingIndicator';
 import FormWizard from '../library/components/FormWizard';
 import StandardActionButton from '../library/components/StandardActionButton';
-import { calculateQuestions } from '../library/helpers/RSVP';
+import { calculateQuestions, formatRSVP } from '../library/helpers/RSVP';
 import DismissKeyboard from '../components/DismissKeyboard';
 import FormOverview from '../library/components/FormOverview';
 import useAnimatedWizard from '../library/hooks/useAnimatedWizard';
 import useLazyQuery from '../library/hooks/useLazyQuery';
+import parseError from '../library/helpers/parseError';
 
 const { width } = Dimensions.get('window');
 const GET_RSVP_QUESTIONS = loader('../graphql/queries/getRSVPQuestions.graphql');
+const SUBMIT_RSVP_FORM = loader('../graphql/mutations/submitRSVP.graphql');
 
 const SubmitRSVPScreen = ({ navigation }) => {
   const [questions, setQuestions] = useState([]);
@@ -26,8 +28,10 @@ const SubmitRSVPScreen = ({ navigation }) => {
   const [formError, setFormError] = useState(null);
   const [showOverview, setShowOverview] = useState(false);
   const [fetchRSVPQuestions, { loading, error }] = useLazyQuery(GET_RSVP_QUESTIONS);
+  const [submitRSVPForm, { loading: submitting }] = useMutation(SUBMIT_RSVP_FORM);
   const { colors } = useTheme();
 
+  const isLoading = loading || submitting;
   const currAnswer = rsvpForm[currQuestion?._id];
   const { animateStepChange: animateRSVPStep, animatedWizardStyle } = useAnimatedWizard();
   const { prevQuestion, nextQuestion } = calculateQuestions({
@@ -57,6 +61,20 @@ const SubmitRSVPScreen = ({ navigation }) => {
     fetchDataOnMount();
   }, []);
 
+  const onSubmit = async () => {
+    try {
+      const formattedRSVP = formatRSVP(rsvpForm, questionHistory);
+      const { data } = await submitRSVPForm({ variables: { input: { rsvpForm: formattedRSVP } } });
+      const { submitRSVPForm: formResponse } = data || {};
+
+      if (!formResponse.success) console.error(formResponse.message);
+      else console.log(formResponse.message);
+    } catch (err) {
+      const { message } = parseError(err);
+      console.error(message);
+    }
+  };
+
   const onNext = () => {
     if (!currAnswer) {
       setFormError('Please select an answer before continuing.');
@@ -64,7 +82,7 @@ const SubmitRSVPScreen = ({ navigation }) => {
     }
 
     if (showOverview) {
-      // Submit Answers
+      onSubmit();
     } else if (nextQuestion) {
       setQuestionHistory([...questionHistory, currQuestion]);
       setCurrQuestion({ ...nextQuestion, number: questionNumber + 1 });
@@ -77,8 +95,14 @@ const SubmitRSVPScreen = ({ navigation }) => {
 
   const onPrev = () => {
     if (showOverview) {
-      setQuestionHistory(questionHistory.slice(0, -1));
-      animateRSVPStep({ fromStep: 1, toStep: 0, callback: () => setShowOverview(false) });
+      animateRSVPStep({
+        fromStep: 1,
+        toStep: 0,
+        callback: () => {
+          setQuestionHistory(questionHistory.slice(0, -1));
+          setShowOverview(false);
+        },
+      });
     } else if (prevQuestion) {
       setQuestionHistory(questionHistory.slice(0, -1));
       setCurrQuestion({ ...prevQuestion });
@@ -109,8 +133,8 @@ const SubmitRSVPScreen = ({ navigation }) => {
     <View style={styles.container}>
       <DismissKeyboard>
         <View style={[styles.card, { backgroundColor: colors.card }]}>
-          {loading && <LoadingIndicator size={100} />}
-          {!loading && !error && (
+          {isLoading && <LoadingIndicator size={100} />}
+          {!isLoading && !error && (
             <Animated.View style={[styles.rsvpFormContainer, animatedWizardStyle]}>
               {currQuestion && !showOverview && (
                 <FormWizard question={currQuestion} setFormValue={setFormValue} formValues={rsvpForm} />
