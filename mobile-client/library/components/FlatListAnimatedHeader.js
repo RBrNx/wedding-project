0e3b4c02554f2bd48acb1c6409c9cc91fbaf8639
Conor Watson <conor.watson94@gmail.com>
2021-02-14 17:02:17 +0000

    refactor: FlatListAnimatedHeader now uses Reanimated

--- mobile-client/library/components/FlatListAnimatedHeader.js
+++ mobile-client/library/components/FlatListAnimatedHeader.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-import { Animated, StyleSheet, Text, RefreshControl, StatusBar, View } from 'react-native';
+import { StyleSheet, Text, RefreshControl, StatusBar, View, FlatList } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 import useCustomScrollbar from '../hooks/useCustomScrollbar';
 import CustomScrollbar from './CustomScrollbar';
 
 const HEADER_MAX_HEIGHT = 350;
 const HEADER_MIN_HEIGHT = 100;
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 const FlatListAnimatedHeader = ({
   title,
@@ -18,56 +27,96 @@ const FlatListAnimatedHeader = ({
   ListFooterComponent,
 }) => {
   const { colors } = useTheme();
-  const [scrollY] = useState(new Animated.Value(0));
+  // const [scrollY] = useState(new Animated.Value(0));
+  const scrollY = useSharedValue(0);
   const [isRefreshing, setIsRefreshing] = useState(false);
-  const {
-    setTotalScrollbarHeight,
-    setVisibleScrollbarHeight,
-    scrollbarHandlePosition,
-    scrollbarHandleSize,
-    scrollbarOpacity,
-    showScrollbar,
-    hideScrollbar,
-  } = useCustomScrollbar(scrollY);
+  // const {
+  //   setTotalScrollbarHeight,
+  //   setVisibleScrollbarHeight,
+  //   scrollbarHandlePosition,
+  //   scrollbarHandleSize,
+  //   scrollbarOpacity,
+  //   showScrollbar,
+  //   hideScrollbar,
+  // } = useCustomScrollbar(scrollY);
 
   const isFlatlistEmpty = !data?.length;
 
-  const imageOpacity = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
-    outputRange: [1, 1, 0],
-    extrapolate: 'clamp',
+  // const imageOpacity = scrollY.interpolate({
+  //   inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+  //   outputRange: [1, 1, 0],
+  //   extrapolate: 'clamp',
+  // });
+
+  // const imageScale = scrollY.interpolate({
+  //   inputRange: [0, HEADER_SCROLL_DISTANCE],
+  //   outputRange: [1, 0.2],
+  //   extrapolate: 'clamp',
+  // });
+
+  // const titleOpacity = scrollY.interpolate({
+  //   inputRange: [0, HEADER_SCROLL_DISTANCE * 0.75, HEADER_SCROLL_DISTANCE],
+  //   outputRange: [0, 0.5, 1],
+  //   extrapolate: 'clamp',
+  // });
+
+  // const translateY = scrollY.interpolate({
+  //   inputRange: [0, HEADER_SCROLL_DISTANCE],
+  //   outputRange: [0, -HEADER_SCROLL_DISTANCE],
+  //   extrapolate: 'clamp',
+  // });
+
+  // const scrollBarTop = scrollY.interpolate({
+  //   inputRange: [0, HEADER_SCROLL_DISTANCE],
+  //   outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+  //   extrapolate: 'clamp',
+  // });
+
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;
   });
 
-  const imageScale = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [1, 0.2],
-    extrapolate: 'clamp',
+  const animatedHandleStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      scrollY.value,
+      [0, HEADER_SCROLL_DISTANCE],
+      [0, -HEADER_SCROLL_DISTANCE],
+      Extrapolate.CLAMP,
+    );
+
+    return {
+      transform: [{ translateY }],
+    };
   });
 
-  const titleOpacity = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE * 0.75, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, 0.5, 1],
-    extrapolate: 'clamp',
-  });
+  const animatedNavBarStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(
+      scrollY.value,
+      [0, HEADER_SCROLL_DISTANCE * 0.75, HEADER_SCROLL_DISTANCE],
+      [0, 0.5, 1],
+      Extrapolate.CLAMP,
+    ),
+  }));
 
-  const translateY = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, -HEADER_SCROLL_DISTANCE],
-    extrapolate: 'clamp',
-  });
+  const animatedImageStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      scrollY.value,
+      [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      [1, 1, 0],
+      Extrapolate.CLAMP,
+    );
+    const scale = interpolate(scrollY.value, [0, HEADER_SCROLL_DISTANCE], [1, 0.2], Extrapolate.CLAMP);
 
-  const scrollBarTop = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-    extrapolate: 'clamp',
+    return {
+      opacity,
+      transform: [{ scale }],
+    };
   });
 
   const renderListHandle = () => {
     return (
-      <Animated.View
-        style={[styles.handleContainer, { transform: [{ translateY }], backgroundColor: colors.background }]}
-      >
-        <View style={[styles.handleBackgound, { backgroundColor: colors.componentBackground }]}>
+      <Animated.View style={[styles.handleContainer, animatedHandleStyle, { backgroundColor: colors.background }]}>
+        <View style={[styles.handleBackgound, { backgroundColor: colors.cardBackround }]}>
           <View style={styles.handle} />
         </View>
       </Animated.View>
@@ -94,19 +143,17 @@ const FlatListAnimatedHeader = ({
   return (
     <View style={styles.container}>
       <Animated.View style={styles.headerContainer} pointerEvents='none'>
-        <Animated.View style={[styles.headerImage, { opacity: imageOpacity, transform: [{ scale: imageScale }] }]}>
-          {renderImage && renderImage()}
-        </Animated.View>
+        <Animated.View style={[styles.headerImage, animatedImageStyle]}>{renderImage && renderImage()}</Animated.View>
         <Text style={styles.headerTitle}>{title}</Text>
       </Animated.View>
       <View style={styles.flatlistContainer}>
-        <Animated.FlatList
+        <AnimatedFlatList
           contentContainerStyle={[
             isFlatlistEmpty ? styles.flatlistContentEmpty : styles.flatlistContent,
-            { backgroundColor: colors.componentBackground },
+            { backgroundColor: colors.cardBackround },
           ]}
           scrollEventThrottle={1}
-          onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], { useNativeDriver: true })}
+          onScroll={scrollHandler}
           renderItem={renderItem}
           data={data}
           keyExtractor={item => item._id}
@@ -117,26 +164,26 @@ const FlatListAnimatedHeader = ({
           stickyHeaderIndices={[0]}
           refreshControl={renderRefreshControl()}
           showsVerticalScrollIndicator={false}
-          onContentSizeChange={(contentWidth, contentHeight) => {
-            setTotalScrollbarHeight(contentHeight);
-          }}
-          onLayout={e => {
-            setVisibleScrollbarHeight(e.nativeEvent.layout.height);
-          }}
-          onScrollBeginDrag={showScrollbar}
-          onMomentumScrollEnd={hideScrollbar}
+          // onContentSizeChange={(contentWidth, contentHeight) => {
+          //   setTotalScrollbarHeight(contentHeight);
+          // }}
+          // onLayout={e => {
+          //   setVisibleScrollbarHeight(e.nativeEvent.layout.height);
+          // }}
+          // onScrollBeginDrag={showScrollbar}
+          // onMomentumScrollEnd={hideScrollbar}
           scrollEnabled={!isFlatlistEmpty}
         />
-        <CustomScrollbar
+        {/* <CustomScrollbar
           handleSize={scrollbarHandleSize - HEADER_MIN_HEIGHT}
           handlePosition={scrollbarHandlePosition}
           style={{
             transform: [{ translateY: scrollBarTop }],
             opacity: scrollbarOpacity,
           }}
-        />
+        /> */}
       </View>
-      <Animated.View style={[styles.navigationBar, { opacity: titleOpacity, backgroundColor: colors.background }]}>
+      <Animated.View style={[styles.navigationBar, animatedNavBarStyle, { backgroundColor: colors.background }]}>
         <Text style={styles.barTitle}>{title}</Text>
       </Animated.View>
     </View>
