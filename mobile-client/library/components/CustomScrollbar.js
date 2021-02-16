9b5bc7f92b94e83505840a33cb2e576a38ed0441
Conor Watson <conor.watson94@gmail.com>
2021-02-16 18:26:17 +0000

    fix: add proper spacing to CustomScrollbar top + bottom

--- mobile-client/library/components/CustomScrollbar.js
+++ mobile-client/library/components/CustomScrollbar.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
 
+const scrollbarSpacing = 10;
+
 const CustomScrollbar = ({ handleSize, animatedHandleStyle, style, handleStyle }) => {
   return (
     <Animated.View style={[styles.scrollbarContainer, style]}>
-      <Animated.View style={[styles.scrollbarHandle, { height: handleSize }, animatedHandleStyle, handleStyle]} />
+      <Animated.View
+        style={[
+          styles.scrollbarHandle,
+          { height: handleSize - scrollbarSpacing * 2 },
+          animatedHandleStyle,
+          handleStyle,
+        ]}
+      />
     </Animated.View>
   );
 };
@@ -14,15 +23,16 @@ const styles = StyleSheet.create({
   scrollbarContainer: {
     position: 'absolute',
     right: 2,
-    top: 10,
     width: 6,
     borderRadius: 8,
     backgroundColor: 'transparent',
+    height: '100%',
   },
   scrollbarHandle: {
     width: 2,
     borderRadius: 8,
     backgroundColor: 'darkgray',
+    marginTop: scrollbarSpacing,
   },
 });
 
