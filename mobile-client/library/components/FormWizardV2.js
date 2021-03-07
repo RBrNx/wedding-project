04e934acdc93d56fc717e380c9c9ba00b6f5df57
Conor Watson <conor.watson94@gmail.com>
2021-03-07 12:05:53 +0000

    fix: dont attempt to render a step if it is falsey

--- mobile-client/library/components/FormWizardV2.js
+++ mobile-client/library/components/FormWizardV2.js
@@ -8,7 +8,7 @@ const FormWizardV2 = ({ steps = [], renderStep = () => null, renderCount = 3, an
     <View style={styles.container}>
       {steps.map((step, index) => {
         const indexOutOfRange = Math.abs(index - animIndex.value > renderCount);
-        if (indexOutOfRange) return null;
+        if (indexOutOfRange || !step) return null;
 
         return renderStep({ step, index });
       })}
