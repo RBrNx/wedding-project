8a7ba4a72cc154032bdcf9d99ee35c9e0364ac25
Conor Watson <conor.watson94@gmail.com>
2021-03-08 19:14:58 +0000

    chore: move components into their correct folders

--- mobile-client/library/components/DismissKeyboard.js
+++ mobile-client/library/components/DismissKeyboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Keyboard, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 const DismissKeyboard = ({ children, style }) => {
   return (
--- mobile-client/library/components/LoadingIndicator.js
+++ mobile-client/library/components/LoadingIndicator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
 import { View, StyleSheet } from 'react-native';
-import LoadingAnimation from '../assets/animations/loading.json';
+import LoadingAnimation from '../../assets/animations/loading.json';
 
 const LoadingIndicator = ({ size, style }) => (
   <View style={[styles.container, { height: size || 50, width: size || 50 }, style]}>
