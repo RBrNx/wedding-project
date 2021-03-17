c7651d853357be6dac848590a0cdca6fd2f07932
Conor Watson <conor.watson94@gmail.com>
2021-03-17 11:25:38 +0000

    feat: rewrite ConfettiCannon component to use actual animated values

--- mobile-client/library/components/ConfettiCannon.js
+++ mobile-client/library/components/ConfettiCannon.js
@@ -1,24 +1,84 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Dimensions, Image, StyleSheet, View } from 'react-native';
-import Animated from 'react-native-reanimated';
-import ConfettiImage from '../../assets/confetti.png';
+import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withDelay } from 'react-native-reanimated';
+import ConfettoImage from '../../assets/confetti.png';
+import { colours } from '../../styles/theming';
+import withVelocity from '../animations/withVelocity';
 
-const NUM_CONFETTI = 100;
-const COLORS = ['#00e4b2', '#09aec5', '#107ed5'];
+const NUM_CONFETTI = 50;
+const COLOURS = [colours.lightBlue, colours.coral];
 const CONFETTI_SIZE = 16;
+const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
 
 const createConfetti = () => {
-  const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
-
   return [...new Array(NUM_CONFETTI)].map((_, i) => {
     return {
       key: i,
-      x: screenWidth * Math.random(),
-      y: screenHeight * Math.random(),
-      angle: 360 * Math.random(),
-      color: COLORS[i % COLORS.length],
+      x: screenWidth * (i % 2 ? 0.25 : 0.75) - CONFETTI_SIZE / 2,
+      y: -60,
+      angle: 0,
+      xVel: Math.random() * 400 - 200,
+      yVel: Math.random() * 150 + 150,
+      angleVel: (Math.random() * 3 - 1.5) * Math.PI,
+      delay: Math.floor(i / 10) * 500,
+      elasticity: Math.random() * 0.3 + 0.1,
+      colour: COLOURS[i % COLOURS.length],
     };
   });
+};
+
+const Confetto = ({ colour, animatedParams }) => {
+  const { x, y, angle, xVel, yVel, angleVel, delay, elasticity } = animatedParams;
+  const translateX = useSharedValue(x);
+  const translateY = useSharedValue(y);
+  const rotate = useSharedValue(angle);
+
+  useEffect(() => {
+    translateX.value = withDelay(
+      delay,
+      withVelocity({
+        velocity: xVel,
+        xBounds: { lower: 0, upper: screenWidth - CONFETTI_SIZE },
+        elasticity,
+      }),
+    );
+    translateY.value = withDelay(
+      delay,
+      withVelocity({
+        velocity: yVel,
+        elasticity,
+      }),
+    );
+    rotate.value = withDelay(delay, withVelocity({ velocity: angleVel, elasticity }));
+  }, []);
+
+  /* eslint-disable no-self-assign */
+  useDerivedValue(() => {
+    if (translateY.value > screenHeight) {
+      translateX.value = translateX.value;
+      translateY.value = translateY.value;
+      rotate.value = rotate.value;
+    }
+  });
+  /* eslint-enable no-self-assign */
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+        { rotate: `${rotate.value}rad` },
+        { rotateX: `${rotate.value}rad` },
+        { rotateY: `${rotate.value}rad` },
+      ],
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.confettoContainer, animatedStyle]}>
+      <Image source={ConfettoImage} tintColor={colour} style={styles.confetto} />
+    </Animated.View>
+  );
 };
 
 const ConfettiCannon = () => {
@@ -26,32 +86,20 @@ const ConfettiCannon = () => {
 
   return (
     <View pointerEvents='none' style={StyleSheet.absoluteFill}>
-      {confetti.map(({ key, x, y, angle, color }) => {
-        return (
-          <Animated.View
-            key={key}
-            style={[
-              styles.confettiContainer,
-              {
-                transform: [{ translateX: x }, { translateY: y }, { rotate: `${angle}deg` }],
-              },
-            ]}
-          >
-            <Image source={ConfettiImage} tintColor={color} style={styles.confetti} />
-          </Animated.View>
-        );
+      {confetti.map(({ key, colour, ...animatedParams }) => {
+        return <Confetto key={key} colour={colour} animatedParams={animatedParams} />;
       })}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  confettiContainer: {
+  confettoContainer: {
     position: 'absolute',
     top: 0,
     left: 0,
   },
-  confetti: {
+  confetto: {
     width: CONFETTI_SIZE,
     height: CONFETTI_SIZE,
   },
