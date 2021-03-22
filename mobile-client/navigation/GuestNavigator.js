9f66b8c82592286f2b7685a7958a9143a8ef4b0c
Conor Watson <conor.watson94@gmail.com>
2021-03-22 19:03:41 +0000

    feat: add skeleton Tab Navigator for Guests

--- mobile-client/navigation/GuestNavigator.js
+++ mobile-client/navigation/GuestNavigator.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 import SubmitRSVPScreen from '../screens/SubmitRSVP';
 import RSVPSuccessScreen from '../screens/RSVPSuccess';
+import SettingsScreen from '../screens/Settings';
 import NavigationPresets from '../library/helpers/NavigationPresets';
+import WorkInProgressScreen from '../screens/WorkInProgress';
 
 const Stack = createStackNavigator();
 
@@ -11,9 +15,49 @@ const screenOptions = {
   ...TransitionPresets.SlideFromRightIOS,
 };
 
+const Tab = createBottomTabNavigator();
+
+const GuestHomeNavigator = () => {
+  return (
+    <Tab.Navigator tabBarOptions={NavigationPresets.DefaultTabBar}>
+      <Tab.Screen
+        name='Home'
+        component={WorkInProgressScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name='home' color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name='Memories'
+        component={WorkInProgressScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name='image' color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name='Registry'
+        component={WorkInProgressScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name='gift' color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name='Settings'
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name='settings' color={color} size={size} />,
+          headerShown: true,
+          title: 'Settings',
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
 const GuestNavigator = () => {
   return (
     <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name='GuestHome' component={GuestHomeNavigator} />
       <Stack.Screen name='SubmitRSVP' component={SubmitRSVPScreen} />
       <Stack.Screen name='RSVPSuccess' component={RSVPSuccessScreen} />
     </Stack.Navigator>
