572d72bb51942d5035a572795c2ff9d6de59bb2b
Conor Watson <conor.watson94@gmail.com>
2021-03-22 10:04:30 +0000

    feat: create LandingScreen and add it to UnauthenticatedNavigator

--- mobile-client/navigation/UnauthenticatedNavigator.js
+++ mobile-client/navigation/UnauthenticatedNavigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import SignInScreen from '../screens/SignIn';
 import ScannerScreen from '../screens/Scanner';
+import LandingScreen from '../screens/LandingScreen';
 
 const UnauthenticatedStack = createStackNavigator();
 
@@ -9,11 +10,13 @@ const screenOptions = {
   headerTitleAlign: 'center',
   headerBackTitleVisible: false,
   headerShown: false,
+  ...TransitionPresets.SlideFromRightIOS,
 };
 
 const UnauthenticatedNavigator = () => {
   return (
     <UnauthenticatedStack.Navigator screenOptions={screenOptions}>
+      <UnauthenticatedStack.Screen name='Landing' component={LandingScreen} />
       <UnauthenticatedStack.Screen name='SignIn' component={SignInScreen} />
       <UnauthenticatedStack.Screen name='Scanner' component={ScannerScreen} />
     </UnauthenticatedStack.Navigator>
