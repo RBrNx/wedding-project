8f2377ea9c19cd55729482839d27a01cba98614e
Conor Watson <conor.watson94@gmail.com>
2021-03-23 13:19:38 +0000

    fix: add awaitingResult state to useLazyQuery as the loading state doesn't seem to be updated on subsequent calls

--- mobile-client/library/hooks/useLazyQuery.js
+++ mobile-client/library/hooks/useLazyQuery.js
@@ -1,21 +1,24 @@
 import { useLazyQuery as useLazyQueryApollo } from '@apollo/react-hooks';
-import { useEffect, useRef, useCallback } from 'react';
+import { useEffect, useRef, useCallback, useState } from 'react';
 
 const useLazyQuery = (query, options) => {
+  const [awaitingResult, setAwaitingResult] = useState(false);
   const [execute, result] = useLazyQueryApollo(query, options);
 
   const resolveRef = useRef();
 
   useEffect(() => {
-    if (result.called && !result.loading && resolveRef.current) {
+    if (result.called && !result.loading && resolveRef.current && awaitingResult) {
       resolveRef.current(result);
       resolveRef.current = undefined;
+      setAwaitingResult(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [result.loading, result.called]);
+  }, [result.loading, result.called, awaitingResult]);
 
   const queryLazily = useCallback(
     (variables, context) => {
+      setAwaitingResult(true);
       execute({ variables, context });
 
       return new Promise(resolve => {
