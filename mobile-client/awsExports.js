0a2520c4794a6badc7af1453e265010aedddde3a
Conor Watson <conor.watson94@gmail.com>
2021-03-24 20:39:20 +0000

    chore: temp change to test Github Action

--- mobile-client/awsExports.js
+++ mobile-client/awsExports.js
@@ -4,7 +4,7 @@ export default {
     URL: 'https://dev-api.thewatsonwedding.com/',
   },
   cognito: {
-    REGION: 'eu-west-1',
+    REGION: 'eu-west-2',
     USER_POOL_ID: 'eu-west-2_VOHZXgzkq',
     APP_CLIENT_ID: '2da8cjldfh3er1sgct3hvtf4gq',
     IDENTITY_POOL_ID: 'eu-west-2:66c01974-042f-4f97-96e5-ba44ab071b1f',
