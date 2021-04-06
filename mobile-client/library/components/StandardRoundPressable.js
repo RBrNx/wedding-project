2384533c4b529abeaedc6305471e6e1633eea988
Conor Watson <conor.watson94@gmail.com>
2021-04-06 11:54:31 +0100

    feat: StandardRoundPressable now takes a colour prop and uses it to automatically create a pressed style

--- mobile-client/library/components/StandardRoundPressable.js
+++ mobile-client/library/components/StandardRoundPressable.js
@@ -1,18 +1,19 @@
-import { useTheme } from '@react-navigation/native';
 import Color from 'color';
 import React from 'react';
 import { Pressable, StyleSheet } from 'react-native';
 
-const StandardRoundPressable = ({ size = 56, icon, style, pressedStyle, onPress }) => {
-  const { colors } = useTheme();
-
+const StandardRoundPressable = ({ size = 56, colour, icon, style, pressedStyle, onPress }) => {
   const renderPressableStyles = ({ pressed }) => {
-    const pressableStyles = [styles.pressable, { height: size, width: size, borderRadius: size / 2 }, style];
+    const pressableStyles = [
+      styles.pressable,
+      { height: size, width: size, borderRadius: size / 2, backgroundColor: colour },
+      style,
+    ];
 
     if (pressed)
       pressableStyles.push({
-        backgroundColor: Color(colors.cardHover)
-          .fade(0.5)
+        backgroundColor: Color(colour)
+          .darken(0.25)
           .rgb()
           .toString(),
         ...pressedStyle,
