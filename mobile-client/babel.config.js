305adbf51ca83e305119e89db1614158657e1696
Conor Watson <conor.watson94@gmail.com>
2021-04-19 18:36:15 +0100

    feat: add module resolver for library, context, screens and navigation paths

--- mobile-client/babel.config.js
+++ mobile-client/babel.config.js
@@ -2,6 +2,21 @@ module.exports = api => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components', 'macros', 'react-native-reanimated/plugin'],
+    plugins: [
+      'babel-plugin-styled-components',
+      'macros',
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          alias: {
+            library: './src/library',
+            context: './src/context',
+            screens: './src/screens',
+            navigation: './src/navigation',
+          },
+        },
+      ],
+    ],
   };
 };
