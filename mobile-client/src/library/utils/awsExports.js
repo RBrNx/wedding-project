b9161ecb513a8d70191be4db198f7381cb2c1d2e
Conor Watson <conor.watson94@gmail.com>
2021-04-24 14:22:00 +0100

    fix: update awsExports to point at staging env

--- mobile-client/src/library/utils/awsExports.js
+++ mobile-client/src/library/utils/awsExports.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: 'eu-west-2',
-    URL: 'https://dev-api.thewatsonwedding.com/',
+    URL: 'https://staging-api.thewatsonwedding.com/',
   },
   cognito: {
     REGION: 'eu-west-2',
-    USER_POOL_ID: 'eu-west-2_VOHZXgzkq',
-    APP_CLIENT_ID: '2da8cjldfh3er1sgct3hvtf4gq',
-    IDENTITY_POOL_ID: 'eu-west-2:66c01974-042f-4f97-96e5-ba44ab071b1f',
+    USER_POOL_ID: 'eu-west-2_jDlY6S7DP',
+    APP_CLIENT_ID: '7idc6c350tcq03fp0ous09rjih',
+    IDENTITY_POOL_ID: 'eu-west-2:528001f3-0609-4bc0-8ad4-b2d1555566a7',
   },
 };
