cc31c5681294ce527ed427bc1f8cee5dff9a58e8
Conor Watson <conor.watson94@gmail.com>
2021-05-06 10:53:12 +0100

    feat: submitRSVPForm mutation will now update the spotify playlist

--- wedding-api/src/graphql/resolvers/RSVPResponse.js
+++ wedding-api/src/graphql/resolvers/RSVPResponse.js
@@ -1,19 +1,43 @@
-const submitRSVPForm = async (parent, args, { currentUser, db }) => {
+import { QuestionType } from '../../lib/enums';
+
+const submitRSVPForm = async (parent, args, { currentUser, db, dataSources }) => {
   const { input } = args;
   const { rsvpForm } = input;
   const { _id, eventId } = currentUser;
 
   try {
     const RSVPResponseModel = db.model('RSVPResponse');
+    const EventModel = db.model('Event');
+    const QuestionModel = db.model('Question');
 
-    const rsvpExists = await RSVPResponseModel.exists({ userId: _id });
-    let rsvpDoc;
+    const existingRSVP = await RSVPResponseModel.findOne({ userId: _id }).populate('rsvpForm.question');
 
-    if (rsvpExists) {
-      rsvpDoc = await RSVPResponseModel.updateOne({ userId: _id }, { rsvpForm });
+    if (existingRSVP) {
+      await RSVPResponseModel.updateOne({ userId: _id }, { rsvpForm });
     } else {
-      rsvpDoc = new RSVPResponseModel({ userId: _id, eventId, rsvpForm });
+      const rsvpDoc = new RSVPResponseModel({ userId: _id, eventId, rsvpForm });
       await rsvpDoc.save();
+    }
+
+    const songRequestQuestion = await QuestionModel.findOne({ type: QuestionType.SONG_REQUEST, eventId });
+    const songRequestAnswer = rsvpForm.find(({ question }) => question === songRequestQuestion._id.toString())?.answer;
+    const { spotifyConfig } = await EventModel.findById(eventId);
+
+    if (songRequestQuestion) {
+      if (existingRSVP) {
+        const existingTrackUri = existingRSVP.rsvpForm.find(
+          ({ question }) => question === songRequestQuestion._id.toString(),
+        )?.answer;
+        await dataSources.spotifyAPI.removeTrackFromPlaylist({
+          playlistId: spotifyConfig.playlistId,
+          trackUri: existingTrackUri.value,
+        });
+      }
+
+      await dataSources.spotifyAPI.addTrackToPlaylist({
+        playlistId: spotifyConfig.playlistId,
+        trackUri: songRequestAnswer.value,
+      });
     }
 
     return {
