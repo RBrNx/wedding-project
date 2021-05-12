41150e6897492b23f9d52a6b54d0ae97b131c39f
Conor Watson <conor.watson94@gmail.com>
2021-05-12 10:37:58 +0100

    feat: added support for a loading indicator in CachedImage and cacheKey is now a hash generated from the URI

--- mobile-client/src/library/components/CachedImage.js
+++ mobile-client/src/library/components/CachedImage.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Image } from 'react-native';
 import * as FileSystem from 'expo-file-system';
+import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import styled from 'styled-components';
+import { Layout } from 'library/styles';
+import { hash } from 'features/Memories/helpers';
 
-const CachedImage = ({ source: { uri }, cacheKey, ...otherProps }) => {
+const CachedImage = ({ source: { uri }, loadingComponent, style, ...otherProps }) => {
+  const cacheKey = uri ? hash(uri) : null;
   const filesystemURI = `${FileSystem.cacheDirectory}images/${cacheKey}`;
+  const [loading, setLoading] = useState(true);
   const [imgURI, setImgURI] = useState(filesystemURI);
   const componentIsMounted = useRef(true);
+  const loaderVisibility = useSharedValue(1);
+
+  const loaderAnimatedStyle = useAnimatedStyle(() => ({
+    opacity: loaderVisibility.value,
+  }));
 
   useEffect(() => {
     const loadImage = async ({ fileURI }) => {
       try {
         // Use the cached image if it exists
         const metadata = await FileSystem.getInfoAsync(fileURI);
-        if (!metadata.exists) {
+        if (!metadata.exists && uri) {
           // download to cache
           if (componentIsMounted.current) {
             setImgURI(null);
@@ -36,14 +46,28 @@ const CachedImage = ({ source: { uri }, cacheKey, ...otherProps }) => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <Image
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...otherProps}
-      source={{
-        uri: imgURI,
-      }}
-    />
+    <>
+      {loading && <LoaderContainer style={loaderAnimatedStyle}>{loadingComponent}</LoaderContainer>}
+      <Animated.Image
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...otherProps}
+        source={uri ? { uri: imgURI } : null}
+        style={style}
+        onLoad={() => {
+          if (uri) {
+            loaderVisibility.value = withTiming(0, { duration: 200 }, isFinished => {
+              if (isFinished) runOnJS(setLoading)(false);
+            });
+          }
+        }}
+      />
+    </>
   );
 };
+
+const LoaderContainer = styled(Animated.View)`
+  ${Layout.absoluteFill};
+  z-index: 99;
+`;
 
 export default CachedImage;
