413e697c8f3b2f4065a8b9125781b8bcfed5f040
Conor Watson <conor.watson94@gmail.com>
2021-06-16 21:15:27 +0100

    feat: use @gorham/portal to display image gallery above tab navigator

--- mobile-client/src/features/Memories/MemoriesScreen.js
+++ mobile-client/src/features/Memories/MemoriesScreen.js
@@ -1,24 +1,21 @@
 import { Layout } from 'library/styles';
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
+import { Portal } from '@gorhom/portal';
 import MemoriesGrid from './components/MemoriesGrid';
 import ImageGallery from './components/ImageGallery';
 
-const MemoriesScreen = ({ navigation }) => {
+const MemoriesScreen = () => {
   const [selectedAlbum, setSelectedAlbum] = useState(null);
   const images = selectedAlbum || [];
   const galleryVisible = !!images.length;
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      tabBarVisible: !galleryVisible,
-    });
-  }, [galleryVisible]);
-
   return (
     <Container>
       <MemoriesGrid setSelectedAlbum={setSelectedAlbum} galleryVisible={galleryVisible} />
-      <ImageGallery visible={galleryVisible} images={images} onDismiss={() => setSelectedAlbum(null)} />
+      <Portal>
+        <ImageGallery visible={galleryVisible} images={images} onDismiss={() => setSelectedAlbum(null)} />
+      </Portal>
     </Container>
   );
 };
