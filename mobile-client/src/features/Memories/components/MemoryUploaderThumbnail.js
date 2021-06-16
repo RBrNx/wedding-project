4eb7fa4ef0e7fc8d3db7911e18e3f253b50bc6f8
Conor Watson <conor.watson94@gmail.com>
2021-06-16 22:00:48 +0100

    feat: scale uploader thumbnail down when image is selected

--- mobile-client/src/features/Memories/components/MemoryUploaderThumbnail.js
+++ mobile-client/src/features/Memories/components/MemoryUploaderThumbnail.js
@@ -1,16 +1,27 @@
 import StandardPressable from 'library/components/StandardPressable';
 import { Colours, Layout } from 'library/styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Dimensions } from 'react-native';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import styled from 'styled-components/native';
 
 const { width } = Dimensions.get('window');
 
 const MemoryUploaderThumbnail = React.memo(
   ({ asset, isSelected, size, selectedAssetIndex, onThumbnailSelect }) => {
+    const imageScale = useSharedValue(1);
+
+    useEffect(() => {
+      imageScale.value = withTiming(isSelected ? 0.85 : 1, { duration: 500, easing: Easing.out(Easing.exp) });
+    }, [isSelected]);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+      transform: [{ scale: imageScale.value }],
+    }));
+
     return (
       <Container size={size}>
-        <StyledImage source={{ uri: asset.uri }} />
+        <StyledImage source={{ uri: asset.uri }} style={animatedStyle} />
         <SelectorIcon isSelected={isSelected} onPress={() => onThumbnailSelect(asset, isSelected)}>
           {isSelected && <SelectedIndex>{selectedAssetIndex}</SelectedIndex>}
         </SelectorIcon>
@@ -35,6 +46,7 @@ const Container = styled.View`
   margin: 3px;
   height: ${props => props.size}px;
   width: ${props => props.size}px;
+  background-color: rgba(0, 0, 0, 0.15);
 `;
 
 const SelectorIcon = styled(StandardPressable)`
@@ -53,7 +65,7 @@ const SelectedIndex = styled.Text`
   color: ${Colours.neutral.white};
 `;
 
-const StyledImage = styled.Image`
+const StyledImage = styled(Animated.Image)`
   width: 100%;
   height: 100%;
 `;
