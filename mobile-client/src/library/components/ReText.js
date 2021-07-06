68c5a102abb85c145a737ccad1008a66c3f5e66d
Conor Watson <conor.watson94@gmail.com>
2021-07-06 19:22:39 +0100

    feat: updated ReText to handle derived values internally

--- mobile-client/src/library/components/ReText.js
+++ mobile-client/src/library/components/ReText.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { TextInput } from 'react-native';
-import Animated, { useAnimatedProps } from 'react-native-reanimated';
+import Animated, { useAnimatedProps, useDerivedValue } from 'react-native-reanimated';
 import styled from 'styled-components/native';
 
 Animated.addWhitelistedNativeProps({ text: true });
 
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
-const ReText = ({ label, text, style }) => {
+const ReText = ({ label, value, style }) => {
+  const text = useDerivedValue(() => {
+    return `${value.value}`;
+  });
   const animatedProps = useAnimatedProps(() => {
     return {
       text: text.value,
     };
   });
   return (
-    <Container>
-      <Label style={style}>{label}</Label>
-      <StyledTextInput
-        underlineColorAndroid='transparent'
-        editable={false}
-        value={text.value}
-        style={style}
-        {...{ animatedProps }}
-      />
+    <Container style={style}>
+      <Label>{label}</Label>
+      <StyledTextInput underlineColorAndroid='transparent' editable={false} value={text.value} {...{ animatedProps }} />
     </Container>
   );
 };
