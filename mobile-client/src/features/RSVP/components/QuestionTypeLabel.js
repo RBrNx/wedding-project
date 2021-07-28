668b0aa27d27e25b6a0c8194864bfc404793dd27
Conor Watson <conor.watson94@gmail.com>
2021-07-28 23:10:07 +0100

    refactor: convert QuestionTypeLabel to use StandardPillPressable

--- mobile-client/src/features/RSVP/components/QuestionTypeLabel.js
+++ mobile-client/src/features/RSVP/components/QuestionTypeLabel.js
@@ -1,38 +1,11 @@
-import Color from 'color';
-import StandardPressable from 'library/components/StandardPressable';
+import StandardPillPressable from 'library/components/StandardPillPressable';
 import { QuestionType } from 'library/enums';
-import { Outlines, Theme, Typography } from 'library/styles';
 import React from 'react';
-import styled from 'styled-components';
 
-const QuestionTypeLabel = ({ type, style, pressedStyle, onPress }) => {
+const QuestionTypeLabel = ({ type, style, onPress }) => {
   const { color, text } = QuestionType[type];
 
-  return (
-    <StyledPressable colour={color} style={style} pressedStyle={pressedStyle} onPress={onPress}>
-      <Label>{text}</Label>
-    </StyledPressable>
-  );
+  return <StandardPillPressable colour={color} text={text} style={style} onPress={onPress} />;
 };
-
-const StyledPressable = styled(StandardPressable).attrs({
-  pressedStyle: {
-    opacity: 0.75,
-  },
-})`
-  align-self: flex-start;
-  padding-vertical: 2.5px;
-  padding-horizontal: 10px;
-  margin-vertical: 5px;
-  background-color: ${props => Color(props.colour).fade(0.25)};
-  ${Outlines.borderRadius};
-`;
-
-const Label = styled.Text`
-  ${Typography.small};
-  ${Typography.boldFont};
-  color: ${Theme.card};
-  ${Outlines.borderRadius};
-`;
 
 export default QuestionTypeLabel;
