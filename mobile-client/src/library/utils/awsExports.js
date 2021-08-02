63f3433c7999b3d941c84ba4d9854df5dabd4dcd
Conor Watson <conor.watson94@gmail.com>
2021-08-02 12:21:05 +0100

    chore: update awsExports to dev

--- mobile-client/src/library/utils/awsExports.js
+++ mobile-client/src/library/utils/awsExports.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: 'eu-west-2',
-    URL: 'https://staging-api.thewatsonwedding.com/',
+    URL: 'https://dev-api.thewatsonwedding.com/',
   },
   cognito: {
     REGION: 'eu-west-2',
-    USER_POOL_ID: 'eu-west-2_jDlY6S7DP',
-    APP_CLIENT_ID: '9oh7jtdq9rj8e2eikf2jgh0a3',
-    IDENTITY_POOL_ID: 'eu-west-2:528001f3-0609-4bc0-8ad4-b2d1555566a7',
+    USER_POOL_ID: 'eu-west-2_Wr7XT2nO4',
+    APP_CLIENT_ID: '5d2rik2tphdef7hiv756inkpfj',
+    IDENTITY_POOL_ID: 'eu-west-2:2bd26c55-6537-4023-8cbb-d64fad519dfa',
   },
 };
