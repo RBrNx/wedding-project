03e39eb4b04162bfa027877456c5dfc1870a0374
Conor Watson <conor.watson94@gmail.com>
2021-08-19 11:55:05 +0100

    feat: add PhoneNumber scalar and resolver

--- wedding-api/src/graphql/resolvers/Base.js
+++ wedding-api/src/graphql/resolvers/Base.js
@@ -1,10 +1,18 @@
-import { DateResolver, TimeResolver, DateTimeResolver, EmailAddressResolver, URLResolver } from 'graphql-scalars';
+import {
+  DateResolver,
+  TimeResolver,
+  DateTimeResolver,
+  EmailAddressResolver,
+  URLResolver,
+  PhoneNumberResolver,
+} from 'graphql-scalars';
 
 export default {
   Date: DateResolver,
   Time: TimeResolver,
   DateTime: DateTimeResolver,
   EmailAddress: EmailAddressResolver,
+  PhoneNumber: PhoneNumberResolver,
   URL: URLResolver,
   MutationResponse: {
     __resolveType() {
--- wedding-api/src/graphql/typeDefs/Base.js
+++ wedding-api/src/graphql/typeDefs/Base.js
@@ -9,6 +9,8 @@ const schema = gql`
 
   scalar EmailAddress
 
+  scalar PhoneNumber
+
   scalar URL
 
   interface MutationResponse {
