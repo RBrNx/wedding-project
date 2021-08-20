3d224cda8f0dfba3d00e7b1a95b5b6b4e876f88f
Conor Watson <conor.watson94@gmail.com>
2021-08-20 16:58:09 +0100

    feat: create EditMenuCard, EditScheduleCard and EditVenueCard components to enable admins to add the relevant details

---
+++ mobile-client/src/features/Details/components/EditMenuCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Spacer from 'library/components/Spacer';
+import { Colours, Layout, Outlines, Theme, Typography } from 'library/styles';
+import theme from 'styled-theming';
+import StandardButton from 'library/components/StandardButton';
+
+const EditMenuCard = ({ venue }) => {
+  return (
+    <Card>
+      <HeadingText>Wedding Menu</HeadingText>
+      <Spacer size={25} />
+      {!venue && (
+        <EmptyContainer>
+          <EmptyDescription>Let your guests what the food choices are 🍽️</EmptyDescription>
+          <Spacer size={30} />
+          <StandardButton text='Add Menu' outline onPress={() => {}} />
+        </EmptyContainer>
+      )}
+    </Card>
+  );
+};
+
+const Card = styled.View`
+  width: 100%;
+  padding: 5%;
+  background-color: ${theme('theme', {
+    light: Colours.neutral.offWhite,
+    dark: Colours.neutral.grey5,
+  })};
+  ${Outlines.borderRadius};
+  ${Outlines.boxShadow};
+`;
+
+const HeadingText = styled.Text`
+  ${Typography.h3};
+  color: ${Theme.headerTextColour};
+`;
+
+const EmptyContainer = styled.View`
+  ${Layout.flexCenter};
+`;
+
+const EmptyDescription = styled.Text`
+  ${Typography.h4};
+  color: ${Theme.headerTextColour};
+  text-align: center;
+`;
+
+export default EditMenuCard;
