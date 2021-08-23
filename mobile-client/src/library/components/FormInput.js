7a48f7d80e294dd824b0dd048e7772c1839f5ded
Conor Watson <conor.watson94@gmail.com>
2021-08-23 21:46:21 +0100

    fix: eslint issues

--- mobile-client/src/library/components/FormInput.js
+++ mobile-client/src/library/components/FormInput.js
@@ -3,14 +3,13 @@ import { useFormContext, useController } from 'react-hook-form';
 import StandardTextInput from './StandardTextInput';
 
 const FormInput = ({ name, rules, defaultValue = '', ...inputProps }) => {
-  const { control, errors } = useFormContext();
+  const { control } = useFormContext();
   const { field } = useController({ name, control, rules, defaultValue });
 
   return (
     <StandardTextInput
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...inputProps}
-      // error={errors[name]?.message}
       onChangeText={field.onChange}
       onBlur={field.onBlur}
       value={field.value}
