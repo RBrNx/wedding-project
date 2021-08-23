e7e9a6ea3c930cb698afc5b2638745cc6b2d6634
Conor Watson <conor.watson94@gmail.com>
2021-08-23 11:10:31 +0100

    feat: display schedule in EditScheduleCard

--- mobile-client/src/features/Details/components/EditScheduleCard.js
+++ mobile-client/src/features/Details/components/EditScheduleCard.js
@@ -4,19 +4,40 @@ import Spacer from 'library/components/Spacer';
 import { Colours, Layout, Outlines, Theme, Typography } from 'library/styles';
 import theme from 'styled-theming';
 import StandardButton from 'library/components/StandardButton';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import StandardPressable from 'library/components/StandardPressable';
+import { AntDesign } from '@expo/vector-icons';
 
-const EditScheduleCard = ({ venue }) => {
+dayjs.extend(customParseFormat);
+
+const EditScheduleCard = ({ schedule, onPress }) => {
   return (
     <Card>
-      <HeadingText>Wedding Schedule</HeadingText>
+      <HeadingContainer>
+        <HeadingText>Wedding Schedule</HeadingText>
+        {schedule && (
+          <EditButton size={40} onPress={onPress}>
+            <EditIcon name='edit' size={25} />
+          </EditButton>
+        )}
+      </HeadingContainer>
       <Spacer size={25} />
-      {!venue && (
+      {!schedule && (
         <EmptyContainer>
           <EmptyDescription>Let your guests know the order of events 🍾</EmptyDescription>
           <Spacer size={30} />
-          <StandardButton text='Add Schedule' outline onPress={() => {}} />
+          <StandardButton text='Add Schedule' outline onPress={onPress} />
         </EmptyContainer>
       )}
+      {schedule &&
+        schedule.map(({ time, name }, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <React.Fragment key={index}>
+            <EventText>{`${dayjs(time, 'HH:mm').format('h:mm A')} - ${name}`}</EventText>
+            <Spacer size={5} />
+          </React.Fragment>
+        ))}
     </Card>
   );
 };
@@ -32,9 +53,28 @@ const Card = styled.View`
   ${Outlines.boxShadow};
 `;
 
+const HeadingContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const HeadingText = styled.Text`
   ${Typography.h3};
   color: ${Theme.headerTextColour};
+`;
+
+const EditButton = styled(StandardPressable).attrs(props => ({
+  pressedStyle: {
+    backgroundColor: Theme.cardPressed(props),
+  },
+}))`
+  ${Layout.flexCenter};
+  ${props => Layout.round(props.size)}
+`;
+
+const EditIcon = styled(AntDesign)`
+  color: ${Theme.icon};
 `;
 
 const EmptyContainer = styled.View`
@@ -45,6 +85,11 @@ const EmptyDescription = styled.Text`
   ${Typography.h4};
   color: ${Theme.headerTextColour};
   text-align: center;
+`;
+
+const EventText = styled.Text`
+  ${Typography.body};
+  color: ${Theme.bodyTextColour};
 `;
 
 export default EditScheduleCard;
