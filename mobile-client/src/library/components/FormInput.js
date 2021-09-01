d8e1da7ecacc2e461efe67db0516727911e7af8b
Conor Watson <conor.watson94@gmail.com>
2021-09-01 20:33:07 +0100

    feat: add support for StandardEnumSelect and Switch inputs to FormInput component

--- mobile-client/src/library/components/FormInput.js
+++ mobile-client/src/library/components/FormInput.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useFormContext, useController } from 'react-hook-form';
+import { Switch } from 'react-native';
+import StandardEnumSelect from './StandardEnumSelect';
 import StandardTextInput from './StandardTextInput';
 
-const FormInput = ({ name, rules, defaultValue = '', ...inputProps }) => {
+const FormInput = ({ name, rules, defaultValue = '', type, ...inputProps }) => {
   const { control } = useFormContext();
   const { field } = useController({ name, control, rules, defaultValue });
+
+  if (type === 'EnumSelect') {
+    return <StandardEnumSelect value={field.value} onSelect={field.onChange} enumObject={inputProps.enumObject} />;
+  }
+
+  if (type === 'Switch') {
+    return <Switch value={field.value} onValueChange={field.onChange} />;
+  }
 
   return (
     <StandardTextInput
