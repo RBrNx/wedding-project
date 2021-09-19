0b7d4a65963ad5386358155cead83c98de513761
Conor Watson <conor.watson94@gmail.com>
2021-09-19 10:24:14 +0100

    feat: add error messages to FormInput

--- mobile-client/src/library/components/FormInput.js
+++ mobile-client/src/library/components/FormInput.js
@@ -1,28 +1,49 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { Colours, Outlines, Theme } from 'library/styles';
 import React from 'react';
-import { useFormContext, useController } from 'react-hook-form';
+import { useFormContext, useController, useFormState, get } from 'react-hook-form';
 import { Switch } from 'react-native';
+import styled from 'styled-components';
 import StandardEnumSelect from './StandardEnumSelect';
 import StandardSelectInput from './StandardSelectInput';
 import StandardTextInput from './StandardTextInput';
 
-const FormInput = ({ name, rules, defaultValue = '', type, ...inputProps }) => {
+const FormInput = ({ name, rules, defaultValue = '', type = 'Text', ...inputProps }) => {
   const { control } = useFormContext();
   const { field } = useController({ name, control, rules, defaultValue });
+  const { errors } = useFormState({ control });
 
-  if (type === 'EnumSelect') {
-    return <StandardEnumSelect value={field.value} onSelect={field.onChange} enumObject={inputProps.enumObject} />;
-  }
+  const error = get(errors, name);
 
-  if (type === 'Switch') {
-    return <Switch value={field.value} onValueChange={field.onChange} />;
-  }
-
-  if (type === 'Select') {
-    return <StandardSelectInput {...inputProps} value={field.value} onChange={field.onChange} />;
-  }
-
-  return <StandardTextInput {...inputProps} onChangeText={field.onChange} value={field.value} />;
+  return (
+    <Container>
+      <>
+        {type === 'EnumSelect' && (
+          <StandardEnumSelect value={field.value} onSelect={field.onChange} enumObject={inputProps.enumObject} />
+        )}
+        {type === 'Switch' && <Switch value={field.value} onValueChange={field.onChange} />}
+        {type === 'Select' && (
+          <StandardSelectInput {...inputProps} value={field.value} onChange={field.onChange} error={!!error} />
+        )}
+        {type === 'Text' && (
+          <StandardTextInput {...inputProps} onChangeText={field.onChange} value={field.value} error={!!error} />
+        )}
+      </>
+      {!!error && <ErrorMessage>{error.message}</ErrorMessage>}
+    </Container>
+  );
 };
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const ErrorMessage = styled.Text`
+  margin-top: 5px;
+  background-color: ${Colours.warning};
+  padding: 2.5%;
+  color: ${Theme.card};
+  ${Outlines.borderRadius};
+`;
 
 export default FormInput;
