4ae08b14b243745341203b92b9eae59a48a09353
Conor Watson <conor.watson94@gmail.com>
2021-10-11 19:11:32 +0100

    feat: add global gql errorLink

--- mobile-client/src/library/utils/apolloClient.js
+++ mobile-client/src/library/utils/apolloClient.js
@@ -3,6 +3,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import Constants from 'expo-constants';
 import { Auth } from 'aws-amplify';
+import { from } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 import awsSigV4Fetch from './awsSigV4Fetch';
 
 const { BASE_API_URL, AUTH_ENDPOINT, UNAUTH_ENDPOINT } = Constants.manifest.extra;
@@ -23,8 +25,17 @@ const httpLink = new HttpLink({
   },
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`),
+    );
+
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
+
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
