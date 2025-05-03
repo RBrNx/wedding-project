bb2b33039e830e11492fc321beaa3167be7e5335
Conor Watson <conor.watson94@gmail.com>
2025-05-03 16:51:01 +0100

    fix(N/A): add eas build requirements

---
+++ mobile-client/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+import Amplify from 'aws-amplify';
+import { setStatusBarStyle } from 'expo-status-bar';
+import * as Sentry from 'sentry-expo';
+import client from 'library/utils/apolloClient';
+import { SettingsProvider, AuthProvider, CurrentThemeProvider, AlertProvider } from 'context';
+import AppNavigator from 'navigation/AppNavigator';
+import awsConfig from 'library/utils/awsExports';
+import allSettled from 'promise.allsettled';
+import { DatastoreProvider } from 'context/Datastore';
+
+allSettled.shim();
+
+Amplify.configure({
+  Auth: {
+    region: awsConfig.cognito.REGION,
+    userPoolId: awsConfig.cognito.USER_POOL_ID,
+    identityPoolId: awsConfig.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: awsConfig.cognito.APP_CLIENT_ID,
+  },
+});
+
+Sentry.init({
+  dsn: 'https://1d8fb9d9632e472ba738538077f16a5e@o439917.ingest.sentry.io/5721792',
+  enableInExpoDevelopment: false,
+  debug: __DEV__,
+});
+
+const App = () => {
+  useEffect(() => {
+    setStatusBarStyle('light');
+  }, []);
+
+  return (
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <DatastoreProvider>
+          <SettingsProvider>
+            <CurrentThemeProvider>
+              {/* <AppLoader> */}
+              <AlertProvider>
+                <AppNavigator />
+              </AlertProvider>
+              {/* </AppLoader> */}
+            </CurrentThemeProvider>
+          </SettingsProvider>
+        </DatastoreProvider>
+      </AuthProvider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
